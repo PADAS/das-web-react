2e8ca1b578e763af243d99113362071c55f54185
Joshua Krautwurst <joshuak@vulcan.com>
2019-03-22 11:24:27 -0700

    code cleanup.

--- src/Badge/index.js
+++ src/Badge/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { STATUSES } from '../constants';
 import styles from './styles.module.scss';
 
-const { HEALTHY_STATUS, WARNING_STATUS, UNHEALTHY_STATUS, UNKNOWN_STATUS } = STATUSES;
+const { HEALTHY_STATUS, WARNING_STATUS, UNHEALTHY_STATUS } = STATUSES;
 
 const calcBadgeColorFromStatus = status => {
   switch (status) {
