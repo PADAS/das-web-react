7514f18a3fba4a42ce0674307ad9928c2383aed4
Joshua Krautwurst <joshuak@vulcan.com>
2019-04-04 14:33:52 -0700

    adding new files

---
+++ src/utils/datetime.js
@@ -0,0 +1,4 @@
+import { subMonths, startOfDay } from 'date-fns';
+
+export const STANDARD_DATE_FORMAT = 'D MMM YYYY hh:mm';
+export const generateOneMonthAgoDate = () => new Date(startOfDay(subMonths(new Date(), 1))).toISOString();
