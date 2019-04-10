bcbdddc101c344ed2386910c9428668d29b5ef66
Joshua Krautwurst <joshuak@vulcan.com>
2019-04-10 16:42:02 -0700

    markup semantics, CSS var API for configuring hamburger menu display props

--- src/DataExportMenu/index.js
+++ src/DataExportMenu/index.js
@@ -11,7 +11,7 @@ export default (props) => {
   const [isOpen, setOpenState] = useState(false)
 
   return <Dropdown onToggle={isOpen => setOpenState(isOpen)} {...rest}>
-    <Toggle>
+    <Toggle as="div">
       <HamburgerMenuIcon isOpen={isOpen} />
     </Toggle>
     <Menu>
