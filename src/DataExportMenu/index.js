cc83f7637079d8e59ac744865c1e389e2cfdaf33
Joshua Krautwurst <joshuak@vulcan.com>
2019-04-10 16:26:58 -0700

    hand-rolled hamburger menu for react since apparently nobody's made a good, accessible one (yet). scaffold for data export menu.

---
+++ src/DataExportMenu/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Dropdown } from 'react-bootstrap';
+
+import HamburgerMenuIcon from '../HamburgerMenuIcon';
+
+const { Toggle, Menu, Item } = Dropdown;
+
+export default (props) => {
+  const { ...rest } = props;
+  const [isOpen, setOpenState] = useState(false)
+
+  return <Dropdown onToggle={isOpen => setOpenState(isOpen)} {...rest}>
+    <Toggle>
+      <HamburgerMenuIcon isOpen={isOpen} />
+    </Toggle>
+    <Menu>
+      <Item>A great menu item indeed</Item>
+    </Menu>
+  </Dropdown>;
+};
