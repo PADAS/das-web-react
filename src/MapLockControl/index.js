20cbd98f01c063e5fad535ef1abfdeea939764d0
Jay Lorenzo <cxplanet@hotmail.com>
2019-06-23 15:32:56 -0700

    Cleanup

--- src/MapLockControl/index.js
+++ src/MapLockControl/index.js
@@ -7,18 +7,18 @@ import { lockMap } from '../utils/map';
 const MapLockControl = (props) => {
 
   const { mapIsLocked, toggleMapLockState, map } = props;
-  const className = mapIsLocked ? 'map-lock' : 'map-unlock';
+  const lockClass = mapIsLocked ? 'locked' : 'unlocked';
+  const hoverText = mapIsLocked ? 'Click to unlock map' : 'Click to lock map';
 
   const handleChange = (e) => {
-    console.log('Toggle map lock state to ' + !mapIsLocked);
     toggleMapLockState(!mapIsLocked);
   };
 
   useEffect( () => lockMap(map, mapIsLocked), [mapIsLocked]);
 
-  return <div class>
-    <input type='checkbox' name='maplock' defaultChecked={mapIsLocked} onChange={handleChange}/>
-    <label htmlFor='maplock'>Lock map movement</label>
+  return <div>
+    <input type='checkbox' name='maplock' checked={mapIsLocked} onChange={handleChange}/>
+    <label htmlFor='maplock'>Lock Map Movement</label>
     </div>;
 };
 
