e7b5b31426757ec6290eccd4d5809962ce1860f0
Jay Lorenzo <cxplanet@hotmail.com>
2019-08-06 09:51:44 -0700

    Change the tracker to be used as an effect

--- src/WithTracker/index.js
+++ src/WithTracker/index.js
@@ -1,44 +1,21 @@
-import React, { Component, } from "react";
+import React, { useEffect } from "react";
 import ReactGA from "react-ga";
-import { REACT_APP_GA_TRACKING_ID } from '../constants';
 
-// Initialize ReactGA with const from .env
-ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
-
-// XXX not using effects here, we can revisit that
 const withTracker = (WrappedComponent, options = {}) => {
-
   const trackPage = page => {
     ReactGA.set({
       page,
-      ...options,
+      ...options
     });
-    console.log('logging pageview' + page);
     ReactGA.pageview(page);
   };
 
-  // eslint-disable-next-line
-  const HOC = class extends Component {
-    componentDidMount() {
-      // eslint-disable-next-line
-      const page = this.props.location.pathname + this.props.location.search;
-      trackPage(page);
-    }
+  const HOC = props => {
+    useEffect(() => trackPage(props.location.pathname), [
+      props.location.pathname
+    ]);
 
-    componentDidUpdate(prevProps) {
-      const currentPage =
-        prevProps.location.pathname + prevProps.location.search;
-      const nextPage =
-        this.props.location.pathname + this.props.location.search;
-
-      if (currentPage !== nextPage) {
-        trackPage(nextPage);
-      }
-    }
-
-    render() {
-      return <WrappedComponent {...this.props} />;
-    }
+    return <WrappedComponent {...props} />;
   };
 
   return HOC;
