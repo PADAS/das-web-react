2511227eb629d8f3f4978a39cc4d0f52adf0ea51
marstvulcan <marst@vulcan.com>
2019-08-30 15:50:16 -0700

    Revert "Implemented Report History Popover"
    This reverts commit 5461bb6891b5b5380536b315c033acae40613431.

--- src/ReportForm/Header.js
+++ src/ReportForm/Header.js
@@ -14,7 +14,6 @@ import EventIcon from '../EventIcon';
 import InlineEditable from '../InlineEditable';
 import HamburgerMenuIcon from '../HamburgerMenuIcon';
 import AddToIncidentModal from './AddToIncidentModal';
-import DateTime from '../DateTime';
 
 import { displayTitleForEventByEventType } from '../utils/events'; 
 import { trackEvent } from '../utils/analytics';
@@ -31,19 +30,16 @@ const calcClassNameForPriority = (priority) => {
 
 const ReportFormHeader = (props) => {
   const { addModal, report, onReportTitleChange, onPrioritySelect, onAddToNewIncident, onAddToExistingIncident } = props;
-  const menuRef = useRef(null);
-  const historyRef = useRef(null);
-  const [headerPopoverOpen, setHeaderPopoverState] = useState(false);
-  const [historyPopoverOpen, setHistoryPopoverState] = useState(false);
-
   const reportTitle = displayTitleForEventByEventType(report);
+  const menuRef = useRef(null);
+
   const reportBelongsToCollection = !!report.is_contained_in && !!report.is_contained_in.length;
   const canAddToIncident = !report.is_collection && !reportBelongsToCollection;
-  const eventOrIncidentReport = `${report.is_collection? 'Incident' : 'Event'} Report`;
+
   const updateTime = report.updated_at || report.created_at;
 
   const onStartAddToIncident = () => {
-    trackEvent(eventOrIncidentReport, `Click 'Add to Incident'`);
+    trackEvent(`${report.is_collection?'Incident':'Event'} Report`, `Click 'Add to Incident'`);
     setHeaderPopoverState(false);
     addModal({
       content: AddToIncidentModal,
@@ -54,57 +50,24 @@ const ReportFormHeader = (props) => {
 
   const onHamburgerMenuIconClick = () => {
     setHeaderPopoverState(!headerPopoverOpen)
-    trackEvent(eventOrIncidentReport, `${headerPopoverOpen?'Close':'Open'} Hamburger Menu`);
+    trackEvent(`${report.is_collection?'Incident':'Event'} Report`, `${headerPopoverOpen?'Close':'Open'}' Hamburger Menu`);
   };
 
-  const onReportHistoryClick = () => {
-    setHistoryPopoverState(!historyPopoverOpen);
-    trackEvent(eventOrIncidentReport, `${historyPopoverOpen?'Close':'Open'} Report History`);
-  };
+  const [headerPopoverOpen, setHeaderPopoverState] = useState(false);
 
-  const ReportHistory = 
-    <span ref={historyRef} onClick={onReportHistoryClick} className={styles.reportHistory}>
-      {report.updates.length > 1? 'Updated' : 'Created'} <TimeAgo date={updateTime}/>
-    </span>
-  ;
-
-  // TODO:Refactor popover className once we standardize popover styles:
-  // remove style.reportPopover and style.popoverHeader
-  const ReportHeaderPopover = <Popover placement='auto' className={styles.reportPopover}>
-    <Popover.Title className={styles.popoverHeader}>{eventOrIncidentReport}</Popover.Title>
-    <Popover.Content className={styles.headerPopoverContent}>
-      <h6>Priority:</h6>
-      <PriorityPicker selected={report.priority} onSelect={onPrioritySelect} />
-      {canAddToIncident && <Fragment>
-        <hr />
-        <Button className={styles.addToIncidentBtn} variant='secondary' onClick={onStartAddToIncident}>
-          <AddToIncidentIcon style={{height: '2rem', width: '2rem'}} />Add to incident
-        </Button>
-      </Fragment>
-      }
-    </Popover.Content>
+  const ReportHeaderPopover = <Popover placement='auto' className={styles.popover}>
+    <h6>Priority:</h6>
+    <PriorityPicker selected={report.priority} onSelect={onPrioritySelect} />
+    <br />
+    {canAddToIncident && <Fragment>
+      <hr />
+      <Button className={styles.addToIncidentBtn} variant='secondary' onClick={onStartAddToIncident}>
+        <AddToIncidentIcon style={{height: '3rem', width: '3rem'}} /> Add to incident
+      </Button>
+    </Fragment>
+    }
   </Popover>;
 
-  // TODO:Refactor popover className once we standardize popover styles:
-  // remove style.reportPopover and style.popoverHeader
-  const ReportHistoryPopover = <Popover placement='right' className={styles.reportPopover}>
-    <Popover.Title className={styles.popoverHeader}>History</Popover.Title>
-    <Popover.Content className={styles.historyPopoverContent}>
-      <ul>
-        {report.updates.map((update) =>
-          <li className={styles.listItem} key={update.time}>
-            <div className={styles.historyItem}>
-              <div className={styles.historyDetails}>
-                <div className={styles.historyMessage}>{update.message.replace(/by (\w+\b)( \w+\b)?$/g, '')}</div>
-                <div className={styles.historyUser}>{`${update.user.first_name} ${update.user.last_name}`.trim()}</div>
-              </div>
-              <DateTime className={styles.historyDate} date={update.time}/>
-            </div>
-          </li>
-        )}
-      </ul>
-    </Popover.Content>
-  </Popover>;
 
   return <div className={`${styles.formHeader} ${styles[calcClassNameForPriority(report.priority)]}`}>
     <h4>
@@ -113,15 +76,12 @@ const ReportFormHeader = (props) => {
       <InlineEditable value={reportTitle} onSave={onReportTitleChange} />
       <div className={styles.headerDetails}>
         <HamburgerMenuIcon ref={menuRef} isOpen={headerPopoverOpen} onClick={onHamburgerMenuIconClick} />
-        <Overlay show={headerPopoverOpen} target={menuRef.current} shouldUpdatePosition={true} 
-          onHide={() => setHeaderPopoverState(false)} placement='auto' rootClose trigger='click'>
+        <Overlay show={headerPopoverOpen} target={menuRef.current} shouldUpdatePosition={true} onHide={() => setHeaderPopoverState(false)} placement='auto' rootClose trigger='click'>
           {ReportHeaderPopover}
         </Overlay>
-        {ReportHistory}          
-        <Overlay show={historyPopoverOpen} target={historyRef.current} shouldUpdatePosition={true} 
-          onHide={() => setHistoryPopoverState(false)} placement='right' rootClose trigger='click'>
-          {ReportHistoryPopover}
-        </Overlay>
+        {updateTime && <small>
+          Updated <TimeAgo date={updateTime} />
+        </small>}
         {report.state === 'resolved' && <small>resolved</small>}
       </div>
     </h4>
