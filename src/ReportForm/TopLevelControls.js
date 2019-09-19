c14078d85b902a7a039bf6ea1087acd7fa7d94b6
Jay Lorenzo <jayl@vulcan.com>
2019-09-19 11:53:54 -0700

    Added ISO format to reports

--- src/ReportForm/TopLevelControls.js
+++ src/ReportForm/TopLevelControls.js
@@ -68,6 +68,7 @@ const ReportFormTopLevelControls = memo((props) => {
         required={true}
         value={report.time ? new Date(report.time) : null}
         maxDate={new Date()}
+        format='yyyy-MM-dd HH:mm'
         onChange={onReportDateChange} />
     </label>
     <label ref={gpsInputLabelRef}>
