c18dc673b5f6888614310e8cf1882868f2b4e2f9
Jay Lorenzo <cxplanet@hotmail.com>
2019-10-08 10:13:23 -0700

    refactor display for list item

--- src/FeatureLayerList/FeatureListItem.js
+++ src/FeatureLayerList/FeatureListItem.js
@@ -13,14 +13,15 @@ import LocationJumpButton from '../LocationJumpButton';
 
 import listStyles from '../SideBar/styles.module.scss';
 
+// eslint-disable-next-line react/display-name
 const FeatureListItem = memo((props) => {
   const { properties, map, geometry, showFeatures } = props;
 
-  const iconForCategory = category => { 
-    if (category === 'geofence') return <GeofenceIcon stroke='black' style={{height: '2rem', width: '2rem'}} />;
-    if (category === 'proximity') return <ProximityIcon stroke='black' style={{height: '2rem', width: '2rem'}} />;
+  const iconForCategory = category => {
+    if (category === 'geofence') return <GeofenceIcon stroke='black' style={{ height: '2rem', width: '2rem' }} />;
+    if (category === 'proximity') return <ProximityIcon stroke='black' style={{ height: '2rem', width: '2rem' }} />;
     return null;
-  }
+  };
 
   const onAnalyzerJumpButtonClick = () => {
     showFeatures(properties.id);
@@ -30,9 +31,9 @@ const FeatureListItem = memo((props) => {
     }, 200);
     const geometry = getAnalyzerAdminPoint(properties.feature_bounds);
     props.showPopup('analyzer-config', { geometry, properties });
-    trackEvent('Map Layers', 'Click Jump To Feature Location button', 
+    trackEvent('Map Layers', 'Click Jump To Analyzer Location button',
       `Feature Type:${properties.type_name}`);
-  }
+  };
 
   const onJumpButtonClick = () => {
     showFeatures(properties.id);
@@ -40,20 +41,16 @@ const FeatureListItem = memo((props) => {
     setTimeout(() => {
       setFeatureActiveStateByID(map, properties.id, true);
     }, 200);
-    trackEvent('Map Layers', 'Click Jump To Feature Location button', 
+    trackEvent('Map Layers', 'Click Jump To Feature Location button',
       `Feature Type:${properties.type_name}`);
-  }
+  };
 
-  // XXX better way to do this?
-  if( properties.analyzer_type) {
-    return <span className={listStyles.featureTitle}>
-            {iconForCategory(properties.analyzer_type)} {properties.title}<LocationJumpButton onButtonClick={onAnalyzerJumpButtonClick} />
-          </span>; 
-  } else {
-    return <span className={listStyles.featureTitle}>
-            {properties.title} <LocationJumpButton onButtonClick={onJumpButtonClick} />
-          </span>; 
-  }
+  const onItemJumpButtonClick = () => properties.analyzer_type ? onAnalyzerJumpButtonClick() : onJumpButtonClick();
+
+  return <span className={listStyles.featureTitle}>
+    {iconForCategory(properties.analyzer_type)} {properties.title}<LocationJumpButton onButtonClick={onItemJumpButtonClick} />
+  </span>;
+
 });
 
 export default connect(null, { showFeatures, showPopup })(FeatureListItem);
