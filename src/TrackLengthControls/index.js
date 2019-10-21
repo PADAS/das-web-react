5c04da8cc4eb1cddd1caf892eb627aa81a0a06f7
Joshua Krautwurst <joshuak@vulcan.com>
2019-10-21 15:09:28 -0700

    track length validation and messaging as per DAS-4446. DDM example and placeholder as per DAS-4398

--- src/TrackLengthControls/index.js
+++ src/TrackLengthControls/index.js
@@ -1,4 +1,5 @@
 import React, { memo, useState, useEffect } from 'react';
+import Alert from 'react-bootstrap/Alert';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Form from 'react-bootstrap/Form';
@@ -15,10 +16,16 @@ const RANGE_INPUT_ATTRS = {
   max: 60,
 };
 
+const FREEHAND_INPUT_ATTRS = {
+  min: RANGE_INPUT_ATTRS.min,
+  max: 365,
+};
+
 const TrackLengthControls = (props) => {
   const { trackLength: { origin, length }, eventFilterTimeRange: { lower, upper }, setTrackLength, setTrackLengthRangeOrigin, onTrackLengthChange } = props;
 
   const [customLengthValue, setCustomLengthValue] = useState(length);
+  const [customLengthValid, setCustomLengthValidity] = useState(true);
   const [initialized, setInitState] = useState(false);
 
   const eventFilterDateRangeLength = differenceInCalendarDays(
@@ -26,26 +33,32 @@ const TrackLengthControls = (props) => {
     lower,
   );
 
-  const setTrackLengthToEventDateRange = () => {
-    setTrackLength(eventFilterDateRangeLength);
-  };
-
   useEffect(() => {
+    const setTrackLengthToEventDateRange = () => {
+      setTrackLength(eventFilterDateRangeLength);
+    };
+  
     if (origin === TRACK_LENGTH_ORIGINS.eventFilter) {
       setTrackLengthToEventDateRange();
     }
-  }, [origin, lower, upper]);
+  }, [origin, lower, upper, setTrackLength, eventFilterDateRangeLength]);
 
   useEffect(() => {
     if (origin === TRACK_LENGTH_ORIGINS.customLength) {
-      setTrackLength(customLengthValue);
+      const rangeIsValid = (customLengthValue >= FREEHAND_INPUT_ATTRS.min) && (customLengthValue <= FREEHAND_INPUT_ATTRS.max);
+      if (rangeIsValid) {
+        setCustomLengthValidity(true);
+        setTrackLength(customLengthValue);
+      } else {
+        setCustomLengthValidity(false);
+      }
     }
-  }, [origin, customLengthValue]);
+  }, [origin, customLengthValue, setTrackLength]);
 
   useEffect(() => {
     if (!initialized) return setInitState(true);
     onTrackLengthChange && onTrackLengthChange(length);
-  }, [length]);
+  }, [initialized, length, onTrackLengthChange]);
 
   const onOriginChange = ({ target: { value } }) => setTrackLengthRangeOrigin(value);
 
@@ -69,8 +82,11 @@ const TrackLengthControls = (props) => {
       <span>Custom length</span>
       <div className={styles.rangeControls}>
         <input type='range' {...RANGE_INPUT_ATTRS} onTouchStart={focusRange} onMouseDown={focusRange} onFocus={focusRange} disabled={origin !== TRACK_LENGTH_ORIGINS.customLength} className={`${styles.rangeSlider} ${origin !== TRACK_LENGTH_ORIGINS.customLength ? styles.disabled : ''}`} value={customLengthValue} onChange={onRangeInputChange} />
-        <input autoComplete='off' min={RANGE_INPUT_ATTRS.min} onTouchStart={focusRange} onMouseDown={focusRange} onFocus={focusRange} disabled={origin !== TRACK_LENGTH_ORIGINS.customLength} className={`${styles.rangeFreeformInput} ${origin !== TRACK_LENGTH_ORIGINS.customLength ? styles.disabled : ''}`} type='number' value={customLengthValue} name='range-freeform-input' onChange={onRangeInputChange} />
+        <input autoComplete='off' {...FREEHAND_INPUT_ATTRS} onTouchStart={focusRange} onMouseDown={focusRange} onFocus={focusRange} disabled={origin !== TRACK_LENGTH_ORIGINS.customLength} className={`${styles.rangeFreeformInput} ${origin !== TRACK_LENGTH_ORIGINS.customLength ? styles.disabled : ''}`} type='number' value={customLengthValue} name='range-freeform-input' onChange={onRangeInputChange} />
       </div>
+      {!customLengthValid && <Alert variant='danger'>
+        Please enter a track length between {FREEHAND_INPUT_ATTRS.min} and {FREEHAND_INPUT_ATTRS.max}.
+      </Alert>}
     </Label>
   </Form>;
 };
