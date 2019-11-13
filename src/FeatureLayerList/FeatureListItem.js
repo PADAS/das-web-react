f0e9f10382841d04451b21e8b6e7c0534a9b6f44
Jay Lorenzo <cxplanet@hotmail.com>
2019-11-13 12:13:09 -0800

    Restore analyzer behavior

--- src/FeatureLayerList/FeatureListItem.js
+++ src/FeatureLayerList/FeatureListItem.js
@@ -4,7 +4,11 @@ import { connect } from 'react-redux';
 import { showFeatures } from '../ducks/map-ui';
 import { showPopup } from '../ducks/popup';
 import { fitMapBoundsToGeoJson, setFeatureActiveStateByID } from '../utils/features';
+import { setAnalyzerFeatureActiveStateForIDs, getAnalyzerAdminPoint, fitMapBoundsForAnalyzer } from '../utils/analyzers';
 import { trackEvent } from '../utils/analytics';
+
+import { ReactComponent as GeofenceIcon } from '../common/images/icons/geofence-analyzer-icon.svg';
+import { ReactComponent as ProximityIcon } from '../common/images/icons/proximity-analyzer-icon.svg';
 import LocationJumpButton from '../LocationJumpButton';
 
 import listStyles from '../SideBar/styles.module.scss';
@@ -13,6 +17,24 @@ import listStyles from '../SideBar/styles.module.scss';
 const FeatureListItem = memo((props) => {
   const { properties, map, geometry, showFeatures } = props;
 
+  const iconForCategory = category => {
+    if (category === 'geofence') return <GeofenceIcon stroke='black' style={{ height: '2rem', width: '2rem' }} />;
+    if (category === 'proximity') return <ProximityIcon stroke='black' style={{ height: '2rem', width: '2rem' }} />;
+    return null;
+  };
+
+  const onAnalyzerJumpButtonClick = () => {
+    showFeatures(properties.id);
+    fitMapBoundsForAnalyzer(map, properties.feature_bounds);
+    setTimeout(() => {
+      const geometry = getAnalyzerAdminPoint(properties.feature_bounds);
+      props.showPopup('analyzer-config', { geometry, properties });
+      setAnalyzerFeatureActiveStateForIDs(map, properties.feature_group, true);
+    }, 200);
+    trackEvent('Map Layers', 'Click Jump To Analyzer Location button',
+      `Feature Type:${properties.type_name}`);
+  };
+ 
   const onJumpButtonClick = () => {
     showFeatures(properties.id);
     fitMapBoundsToGeoJson(map, { geometry });
@@ -23,12 +45,14 @@ const FeatureListItem = memo((props) => {
       `Feature Type:${properties.type_name}`);
   };
 
-  const onMouseOverFeature = (enter) => {
-    setFeatureActiveStateByID(map, properties.id, (enter));
+  const onMouseOverFeature = (enter) => { 
+    setFeatureActiveStateByID(map, properties.id, (enter)); 
   };
 
-  return <span className={listStyles.featureTitle} onMouseEnter={() => onMouseOverFeature(true)} onMouseLeave={() => onMouseOverFeature(false)}>
-    {properties.title}<LocationJumpButton isMulti={false} isFeature={true} map={map} onClick={onJumpButtonClick} />
+  const onItemJumpButtonClick = () => properties.analyzer_type ? onAnalyzerJumpButtonClick() : onJumpButtonClick();
+
+  return <span className={listStyles.featureTitle} onMouseEnter={()=> onMouseOverFeature(true)} onMouseLeave={() => onMouseOverFeature(false)}>
+    {iconForCategory(properties.analyzer_type)} {properties.title}<LocationJumpButton onClick={onItemJumpButtonClick} />
   </span>;
 
 });
