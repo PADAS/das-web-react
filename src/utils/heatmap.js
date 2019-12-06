115055c47da59daa367742d9e39671d5876ef81e
Joshua Krautwurst <joshuak@vulcan.com>
2019-12-06 15:09:22 -0800

    mouseenter/mouseleave functionality for cluster polygons

--- src/utils/heatmap.js
+++ src/utils/heatmap.js
@@ -1,8 +1,4 @@
-const metersPerPixel = (lat, zoom) => {
-  var earthCircumference = 40075017;
-  var latitudeRadians = lat * (Math.PI/180);
-  return earthCircumference * Math.cos(latitudeRadians) / Math.pow(2, zoom + 8);
-};
+import { metersPerPixel } from './map';
 
 export const calcPixelsForMeters = function(meters, map) {
   const { lat } = map.getCenter();
