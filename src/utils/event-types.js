5c7066d7293f9238b640374dec5af8466f8dda29
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2020-01-16 10:11:55 -0800

    Revert "Revert "Feature/4746 add report layout cleanup""

--- src/utils/event-types.js
+++ src/utils/event-types.js
@@ -53,5 +53,5 @@ export const mapReportTypesToCategories = eventTypes => eventTypes
     }
     return accumulator;
   }, [])
-  .sort((item1, item2) => item1.orderNum - item2.orderNum)
+  .sort((item1, item2) => item1.ordernum - item2.ordernum)
   .filter(item => !!item.types.length);
