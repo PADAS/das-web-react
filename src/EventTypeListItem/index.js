b27a51b71af6bd505325e2b9375b02df80646960
Joshua Krautwurst <joshuak@vulcan.com>
2020-02-11 16:19:13 -0800

    wiring a few changes for more stable and expected image results for map symbol features and report types in the 'add report' component

--- src/EventTypeListItem/index.js
+++ src/EventTypeListItem/index.js
@@ -5,9 +5,9 @@ import { calcIconColorByPriority } from '../utils/event-types';
 import DasIcon from '../DasIcon';
 
 const EventTypeListItem = (props) => {
-  const { display, default_priority, value } = props;
+  const { display, default_priority, icon_id, value } = props;
   return <span>
-    <DasIcon type='events' color={calcIconColorByPriority(default_priority)} iconId={value} />
+    <DasIcon type='events' color={calcIconColorByPriority(default_priority)} iconId={icon_id || value} />
     {display}
   </span>;
 };
