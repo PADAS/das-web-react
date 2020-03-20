3ad123cfeb8ece7770e0f5d7ed9c9acddcab406d
blac-siren <husnein.zac@gmail.com>
2020-03-20 18:36:19 +0300

    add whitespace

--- src/TimeAgo/index.js
+++ src/TimeAgo/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { default as TA } from 'react-timeago';
 import { generateCurrentTimeZoneTitle } from '../utils/datetime';
 
-const formatter = (val, unit, suffix) => `${val}${unit === 'month' ? (val > 1) ? unit.substring(0, 2).concat('s') : unit.substring(0, 2) : unit.charAt(0)} ${suffix}`;
+const whiteSpace = (str, space) => str.padStart(space+str.length, " ")
+const formatter = (val, unit, suffix) => `${val}${unit === 'month' ? (val > 1) ? whiteSpace(unit.substring(0, 2).concat('s'), 1) : unit.substring(0, 2) : unit.charAt(0)} ${suffix}`;
 
 const TimeAgo = (props) => <TA formatter={formatter} title={generateCurrentTimeZoneTitle()} {...props} />;
 
