a593a171ed4eaef97ef920a1a94e53ca11b43bce
JayLVulcan <jayl@vulcan.com>
2020-04-22 17:32:28 -0700

    Remove capitalize function

--- src/utils/string.js
+++ src/utils/string.js
@@ -3,5 +3,3 @@ import uuidv4 from 'uuid/v4';
 export const fileNameFromPath = path => path.match(/[^\\/]+$/)[0];
 export const uuid = () => uuidv4();
 export const caseInsensitiveCompare = (str1, str2) => str1.toLowerCase() === str2.toLowerCase();
-// capitalize first letter of a (multi) word sequence, vs the capitalize behavior of css
-export const capitalize = capText => capText.charAt(0).toUpperCase() + capText.slice(1);
