a6da9f0285b762b0f561056c563ba48e97dd5a03
Joshua Krautwurst <joshuak@vulcan.com>
2020-05-29 08:27:57 -0700

    accessing the namednodemap of img attributes to re-render a report image in an HTML marker without rebuilding the element.

--- src/SpideredReportMarker/index.js
+++ src/SpideredReportMarker/index.js
@@ -1,15 +1,42 @@
-import React, { Fragment, memo } from 'react';
-import EventIcon from '../EventIcon';
+import React, { createElement, Fragment, memo, useEffect, useRef, useState } from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { connect } from 'react-redux';
+
+import { calcImgIdFromUrlForMapImages } from '../utils/img';
 
 const SpideredReportMarker = (props) => {
-  const { report } = props;
+  const { mapImages, report } = props;
+  const [imageAttributes, setImgAttributes] = useState(null);
+  
+  useEffect(() => {
+    const storeImage = mapImages[
+      calcImgIdFromUrlForMapImages(
+        (report.image || report.image_url), report.width, report.height,
+      )
+    ];
+    if (storeImage) {
+      const attributes = {};
+      for (var i=0;i<storeImage.attributes.length;i++) {
+        const { nodeName, nodeValue } = storeImage.attributes.item(i);
+        attributes[nodeName] = nodeValue;
+      }
+      setImgAttributes(attributes);
+    }
+  }, [mapImages, report.height, report.image, report.image_url, report.width]);
+
+
+  
   return <Fragment>
-    <EventIcon report={report} /> {/* this should be the <img /> tag from the store rather 
-    than the icon, to match the map presentation */}
+    {!!imageAttributes && <img alt='wow' {...imageAttributes} />}
     {/* calc title with date to go here */}
     {/* style up to be a close match */}
     {/* increase zoom threshold tolerance for declustering, line up w/spiderification behavior */}
   </Fragment>;
 };
 
-export default memo(SpideredReportMarker);
+const mapStateToProps = ({ view: { mapImages } }) => ({
+  mapImages,
+});
+
+
+export default connect(mapStateToProps, null)(memo(SpideredReportMarker));
