a500f2f5e10b5124b01b910625f95960c7ed47a9
Joshua Krautwurst <joshuak@vulcan.com>
2020-07-24 11:00:28 -0700

    adding filterability and layout tweaks to the multi-layer selection popover

--- src/LayerSelectorPopup/index.js
+++ src/LayerSelectorPopup/index.js
@@ -1,37 +1,86 @@
-import React, { memo, useCallback } from 'react';
+import React, { memo, useCallback, useEffect, useMemo, useState } from 'react';
 import { connect } from 'react-redux';
 import { Popup } from 'react-mapbox-gl';
 
 import { hidePopup } from '../ducks/popup';
-import { calcUrlForImage } from '../utils/img';
+import { calcImgIdFromUrlForMapImages, calcUrlForImage } from '../utils/img';
 import { LAYER_IDS } from '../constants';
 
 import styles from './styles.module.scss';
 
 const { EVENT_SYMBOLS } = LAYER_IDS;
 
-const LayerSelectorPopup = ({ id, data, hidePopup }) => {
-  const { layers, onSelectSubject, onSelectEvent, coordinates } = data;
+const LayerSelectorPopup = ({ id, data, hidePopup, mapImages }) => {
+  const { layers:layerList, onSelectSubject, onSelectEvent, coordinates } = data;
+  const [filter, setFilter] = useState('');
 
-  const handleClick = useCallback(({ layer: { id:layerID }, properties, geometry }) => {
+  const onFilterChange = useCallback(({ target: { value } }) => {
+    setFilter(value);
+  }, []);
+
+  const layers = useMemo(() => {
+    if (!filter) return layerList;
+    return layerList.filter((layer) => {
+      const displayName = layer.properties.display_title || layer.properties.name;
+      return displayName.toLowerCase().includes(filter.toLowerCase());
+    });
+  }, [filter, layerList]);
+
+  const itemCountString = useMemo(() => {
+    const subjectCount = layers.filter(({ layer: { id:layerID }}) => !layerID.includes(EVENT_SYMBOLS)).length;
+    const eventCount = layers.filter(({ layer: { id:layerID }}) => layerID.includes(EVENT_SYMBOLS)).length;
+
+    let string = '';
+
+    if (subjectCount) {
+      string+= `${subjectCount} subject${subjectCount > 1 ? 's' : ''}`;
+    }
+    if (subjectCount && eventCount)  {
+      string+= ', ';
+    }
+    if (eventCount) {
+      string+= `${eventCount} report${eventCount > 1 ? 's' : ''}`;
+    }
+    return string;
+  }, [layers]);
+
+  const showFilterInput = useMemo(() => layerList.length > 5, [layerList.length]);
+
+  useEffect(() => {
+    if (!showFilterInput) {
+      setFilter('');
+    }
+  }, [showFilterInput]);
+
+  const handleClick = useCallback((event, { layer: { id:layerID }, properties, geometry }) => {
     const layerData = { geometry, properties };
     const layerType = layerID.includes(EVENT_SYMBOLS) ? 'event' : 'subject';
     const handler = layerType === 'event' ? onSelectEvent : onSelectSubject;
 
     hidePopup(id);
-    handler({ layer: layerData });
+    handler({ event, layer: layerData });
 
   }, [hidePopup, id, onSelectEvent, onSelectSubject]);
 
   return <Popup className={styles.popup} coordinates={coordinates} id='multi-select-popup'>
-    <h5>{layers.length} items at this point</h5>
+    <h6>{itemCountString ? itemCountString : '0 items'} at this point {!!filter && <small>(filtered)</small>}</h6>
+    {showFilterInput && <input type='text' onChange={onFilterChange} placeholder='Type filter text here' className={styles.filterInput} />}
     <ul className={styles.list}>
-      {layers.map(layer => <li className={styles.listItem} key={layer.properties.id} onClick={() => handleClick(layer)}>
-        <img alt={layer.properties.display_title || layer.properties.name} src={calcUrlForImage(layer.properties.image)} />
-        <span>{layer.properties.display_title || layer.properties.name}</span>
-      </li>)}
+      {layers.map(layer => {
+        const imageinStore = mapImages[calcImgIdFromUrlForMapImages(layer.properties.image, layer.properties.height, layer.properties.width)];
+        const imgSrc = imageinStore ?
+          imageinStore.image.src
+          : calcUrlForImage(layer.properties.image);
+      
+        return <li className={styles.listItem} key={layer.properties.id} onClick={(e) => handleClick(e, layer)}>
+          <img alt={layer.properties.display_title || layer.properties.name} src={imgSrc} />
+          <span>{layer.properties.display_title || layer.properties.name}</span>
+        </li>;
+      })}
     </ul>
   </Popup>;
 };
 
-export default connect(null, { hidePopup })(memo(LayerSelectorPopup));
+
+const mapStateToProps = ({ view: { mapImages } }) => ({ mapImages });
+export default connect(mapStateToProps, { hidePopup })(memo(LayerSelectorPopup));
