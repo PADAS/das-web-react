6afd0677288e75e7f1275181c24185586d3ca016
Joshua Krautwurst <joshuak@vulcan.com>
2020-08-03 15:37:06 -0700

    scaffolding for mock api proxy server for patrol requests

---
+++ mock-api/index.js
@@ -0,0 +1,22 @@
+const generateMockPatrols = () => {
+  return [];
+};
+
+const generateMockPatrolTypes = () => {
+  return [];
+};
+
+const generateMockPatrolSegments = () => {
+  return [];
+};
+
+const generateMockPatrolTemplates = () => {
+  return [];
+};
+
+module.exports = () => ({
+  patrols: generateMockPatrols(),
+  patrol_types: generateMockPatrolTypes(),
+  patrol_segments: generateMockPatrolSegments(),
+  patrol_templates: generateMockPatrolTemplates(),
+});
