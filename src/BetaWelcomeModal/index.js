680b278290221d079aee629cc304595e761c22b4
Joshua Krautwurst <joshuak@vulcan.com>
2020-08-31 20:37:12 -0700

    verbage tweaks

--- src/BetaWelcomeModal/index.js
+++ src/BetaWelcomeModal/index.js
@@ -26,12 +26,12 @@ const BetaWelcomeModal = (props) => {
 
   return <Fragment>
     <Header>
-      <Title>EarthRanger Legacy Version Sunset </Title>
+      <Title>EarthRanger Legacy Version Shutdown </Title>
     </Header>
     <Form onSubmit={onClose}>
       <Body style={{maxHeight: '60vh', overflowY: 'scroll'}}>
-        <p>Valued EarthRanger users, We hope you&apos;ve found the new version of EarthRanger launched nearly three months ago to be a more useful tool in helping you with your critically important work!  After working closely with many of you to make ongoing improvements to the new version since its release, we believe it is time to sunset the legacy version.</p>
-        <p>As a result, we will ending access to the legacy version of EarthRanger as of September 30, 2020.  As always, if you have any concerns or feedback, please contact us at feedback@earthranger.com.</p>
+        <p>We hope you&apos;ve found the new version of EarthRanger launched nearly three months ago to be a more useful tool in helping you with your critically important work!  After working closely with many of you to make ongoing improvements to the new version since its release, we believe it is time to shut down the legacy version.</p>
+        <p>As a result, we will be ending access to the legacy version of EarthRanger as of September 30, 2020.  As always, if you have any concerns or feedback, please contact us at <a href="mailto:feedback@earthranger.com">feedback@earthranger.com</a>.</p>
         <p>Thanks again for all you do in protecting the world&apos;s animals and their habitats!</p>
         <p>-The EarthRanger Team</p>
       </Body>
