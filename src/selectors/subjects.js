27f07d6f207217df5cb079841ef8ad220f92b177
Tomisin Abiodun <tomisin.abiodun@andela.com>
2020-11-14 00:57:25 +0100

    Mark subjects that are on patrol

--- src/selectors/subjects.js
+++ src/selectors/subjects.js
@@ -3,23 +3,38 @@ import { tracks } from './tracks';
 
 import { createFeatureCollectionFromSubjects, filterInactiveRadiosFromCollection } from '../utils/map';
 import { getUniqueSubjectGroupSubjects, pinMapSubjectsToVirtualPosition } from '../utils/subjects';
+import { getLeaderForPatrol } from '../utils/patrols';
 
+const getPatrols = ({ data: { patrols: { results } }}) => results;
 const getMapSubjects = ({ data: { mapSubjects: { subjects } } }) => subjects;
 const hiddenSubjectIDs = ({ view: { hiddenSubjectIDs } }) => hiddenSubjectIDs;
 const subjectGroups = ({ data: { subjectGroups } }) => subjectGroups;
 const getSubjectStore = ({ data: { subjectStore } }) => subjectStore;
 const showInactiveRadios = ({ view: { showInactiveRadios } }) => showInactiveRadios;
 
-export const getMapSubjectFeatureCollection = createSelector(
-  [getMapSubjects, getSubjectStore, hiddenSubjectIDs, showInactiveRadios],
-  (mapSubjects, subjectStore, hiddenSubjectIDs, showInactiveRadios) => {
+const getPatrolsForSubject = (patrols, subject) => {
+  return patrols.filter(patrol => {
+    return getLeaderForPatrol(patrol)?.id === subject.id
+  });
+}
 
+export const getMapSubjectFeatureCollection = createSelector(
+  [getMapSubjects, getSubjectStore, hiddenSubjectIDs, showInactiveRadios, getPatrols],
+  (mapSubjects, subjectStore, hiddenSubjectIDs, showInactiveRadios, patrols) => {
     const fromStore = mapSubjects
       .filter(id => !hiddenSubjectIDs.includes(id))
       .map(id => subjectStore[id])
-      .filter(item => !!item);
+      .filter(item => !!item)
+      .map(
+        subject => {
+          const subjectPatrols = getPatrolsForSubject(patrols, subject);
+          subject.is_on_patrol = Boolean(subjectPatrols.length);
+          return subject;
+        }
+      )
     
     const mapSubjectCollection = createFeatureCollectionFromSubjects(fromStore);
+    console.log({mapSubjectCollection});
     if (showInactiveRadios) return mapSubjectCollection;
     return filterInactiveRadiosFromCollection(mapSubjectCollection);
   });
