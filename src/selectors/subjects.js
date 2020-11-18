57e88972c987f059d919f538a7004977e713c94c
Tomisin Abiodun <tomisin.abiodun@andela.com>
2020-11-18 18:54:13 +0100

    Refactor code

--- src/selectors/subjects.js
+++ src/selectors/subjects.js
@@ -3,9 +3,7 @@ import { tracks } from './tracks';
 
 import { createFeatureCollectionFromSubjects, filterInactiveRadiosFromCollection } from '../utils/map';
 import { getUniqueSubjectGroupSubjects, pinMapSubjectsToVirtualPosition } from '../utils/subjects';
-import { getPatrolsForSubject } from '../utils/patrols';
 
-const getPatrols = ({ data: { patrols: { results } }}) => results;
 const getMapSubjects = ({ data: { mapSubjects: { subjects } } }) => subjects;
 const hiddenSubjectIDs = ({ view: { hiddenSubjectIDs } }) => hiddenSubjectIDs;
 const subjectGroups = ({ data: { subjectGroups } }) => subjectGroups;
@@ -13,21 +11,12 @@ const getSubjectStore = ({ data: { subjectStore } }) => subjectStore;
 const showInactiveRadios = ({ view: { showInactiveRadios } }) => showInactiveRadios;
 
 export const getMapSubjectFeatureCollection = createSelector(
-  [getMapSubjects, getSubjectStore, hiddenSubjectIDs, showInactiveRadios, getPatrols],
-  (mapSubjects, subjectStore, hiddenSubjectIDs, showInactiveRadios, patrols) => {
+  [getMapSubjects, getSubjectStore, hiddenSubjectIDs, showInactiveRadios],
+  (mapSubjects, subjectStore, hiddenSubjectIDs, showInactiveRadios) => {
     const fromStore = mapSubjects
       .filter(id => !hiddenSubjectIDs.includes(id))
       .map(id => subjectStore[id])
       .filter(item => !!item)
-      .map(
-        subject => {
-          const subjectPatrols = getPatrolsForSubject(patrols, subject);
-          // console.log({subject, subjectPatrols});
-          // TODO refactor the below line to  check if any of the subjectPatrols are actually active.
-          subject.is_on_active_patrol = Boolean(subjectPatrols.length);
-          return subject;
-        }
-      )
     
     const mapSubjectCollection = createFeatureCollectionFromSubjects(fromStore);
     if (showInactiveRadios) return mapSubjectCollection;
