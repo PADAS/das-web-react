aaf6cc363ff012a994615fd01c264e01b8759b71
Jay Lorenzo <jayl@vulcan.com>
2021-02-03 11:43:18 -0800

    indent filter settings

--- src/PatrolFilter/PatrolFilterSettings.js
+++ src/PatrolFilter/PatrolFilterSettings.js
@@ -7,33 +7,33 @@ const PatrolFilterSettings = (props) => {
   const { handleFilterOptionChange, patrolsOverlapFilter } = props;
 
   return <div className={styles.filterSelection}>
-          <form>
-            <fieldset>
-              <div>
-                <span>
-                  <input
-                    type="radio"
-                    id="start_dates"
-                    value="start_dates"
-                    checked={!patrolsOverlapFilter}
-                    onChange={handleFilterOptionChange}
-                  /><label forHtml="start_dates">Include patrols starting within date range</label>
-                </span>
-              </div>
-              <div>
-                <span>
-                  <input
-                    type="radio"
-                    id="overlap_dates"
-                    value="overlap_dates"
-                    checked={patrolsOverlapFilter}
-                    onChange={handleFilterOptionChange}
-                  /><label htmlFor="overlap_dates">Include patrols whose start to end date range overlaps with date range </label>
-                </span>
-              </div>
-            </fieldset>
-          </form>
-        </div>;
+    <form>
+      <fieldset>
+        <div>
+          <span>
+            <input
+              type="radio"
+              id="start_dates"
+              value="start_dates"
+              checked={!patrolsOverlapFilter}
+              onChange={handleFilterOptionChange}
+            /><label forHtml="start_dates">Include patrols starting within date range</label>
+          </span>
+        </div>
+        <div>
+          <span>
+            <input
+              type="radio"
+              id="overlap_dates"
+              value="overlap_dates"
+              checked={patrolsOverlapFilter}
+              onChange={handleFilterOptionChange}
+            /><label htmlFor="overlap_dates">Include patrols whose start to end date range overlaps with date range </label>
+          </span>
+        </div>
+      </fieldset>
+    </form>
+  </div>;
 };
 
 const mapStateToProps = (state) =>
