70e54c9a5a496b0eecb3680c4cc0ebccbd868eea
Joshua Krautwurst <joshuak@vulcan.com>
2021-02-09 12:41:20 -0800

    using standard status text calculation for patrol modal header status

--- src/PatrolModal/StatusBadge.js
+++ src/PatrolModal/StatusBadge.js
@@ -1,38 +1,17 @@
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
 import { withFormDataContext } from '../EditableItem/context';
 
 import { ReactComponent as Chevron } from '../common/images/icons/triple-chevron.svg';
-import { isPatrolCancelled, isSegmentFinished, 
-  isSegmentOverdue, isSegmentPending, isSegmentActive} from '../utils/patrols';
+import { calcPatrolCardState } from '../utils/patrols';
 
 import styles from './styles.module.scss';
 
 
-const calcPatrolSegmentStatus = (patrol) => {
-  const UNKNOWN_STATUS = 'unknown';
-  
-  const { patrol_segments } = patrol;
-
-  if (!patrol_segments.length) return UNKNOWN_STATUS;
-
-  const [firstLeg]  = patrol_segments;
-
-  if (!firstLeg.time_range) return UNKNOWN_STATUS;
-
-  if (isPatrolCancelled(patrol))return 'canceled';
-  if (isSegmentFinished(firstLeg)) return 'completed';
-  if (isSegmentOverdue(firstLeg)) return 'overdue';
-  if (isSegmentActive(firstLeg)) return 'active';
-  if (isSegmentPending(firstLeg)) return 'pending';
-
-  return UNKNOWN_STATUS;
-};
-
 const StatusBadge = (props) => {
-  const { data } = props;
-  const status = calcPatrolSegmentStatus(data);
+  const { data:patrol } = props;
+  const status = useMemo(() => calcPatrolCardState(patrol), [patrol]);
 
-  return <span className={styles.statusBadge}><Chevron /> {status}</span>;
+  return <span className={styles.statusBadge}><Chevron /> {status.title}</span>;
 };
 
 export default memo(withFormDataContext(StatusBadge));
