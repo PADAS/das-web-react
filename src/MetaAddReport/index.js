37db9cc13c1a3e3bb3372dac596be77b0d2a3157
Joshua Krautwurst <joshuak@vulcan.com>
2021-02-11 13:12:05 -0800

    adding new meta add report component for demo

---
+++ src/MetaAddReport/index.js
@@ -0,0 +1,43 @@
+import React, { useCallback, useEffect, useState, memo } from 'react';
+import AddReport1 from '../AddReport';
+import AddReport2 from '../AddReport2';
+import AddReport3 from '../AddReport3';
+
+const components = [AddReport1, AddReport2, AddReport3];
+
+const MetaAddReport = (props) => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+
+    const bindKeys = (event) => {
+      const { ctrlKey, keyCode } = event;;
+
+      if (!ctrlKey || keyCode !== 78) return;
+
+      const newIndex = index+1;
+
+      if (newIndex >= components.length) {
+        setIndex(0);
+      } else {
+        setIndex(newIndex);
+      }
+    };
+
+    window.addEventListener('keydown', bindKeys);
+    
+    return () => {
+      window.removeEventListener('keydown', bindKeys);
+    };
+    
+  }, [index]);
+
+  const Component = components[index];
+  
+  console.log({ Component });
+
+  return <Component {...props} />;
+
+};
+
+export default memo(MetaAddReport);
