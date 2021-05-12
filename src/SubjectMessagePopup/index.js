8d6064b2794356828bff7557e29574eefd8aac9a
Joshua Krautwurst <joshuak@vulcan.com>
2021-05-12 12:52:20 -0700

    adding date/time to message popup

--- src/SubjectMessagePopup/index.js
+++ src/SubjectMessagePopup/index.js
@@ -2,6 +2,7 @@ import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { Popup } from 'react-mapbox-gl';
 
+import DateTime from '../DateTime';
 import GpsFormatToggle from '../GpsFormatToggle';
 import { ReactComponent as ChatIcon } from '../common/images/icons/chat-icon.svg';
 
@@ -12,7 +13,8 @@ const SubjectMessagePopup = (props) => {
 
   return <Popup /*  offset={[20, 20]} */ coordinates={[message.device_location.longitude, message.device_location.latitude]} id={`message-popup-${message.id}`}>
     <h6><ChatIcon /> {subject.name}</h6>
-    <p>{message.text}</p>
+    <p style={{marginBottom: '0.25rem'}}>{message.text}</p>
+    <DateTime date={message.message_time}  />
     <GpsFormatToggle lng={message.device_location.longitude} lat={message.device_location.latitude} />
   </Popup>;
 };
