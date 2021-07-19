48067736431fbc1ebc1a71abc2f93b52a2bfc670
Joshua Krautwurst <joshuak@vulcan.com>
2021-07-19 14:47:33 -0700

    declarative service worker based pattern for response interception during tests using msw. setting up.

--- src/NotificationMenu/index.test.js
+++ src/NotificationMenu/index.test.js
@@ -1,12 +1,43 @@
 import mocks from '../__test-helpers/mocks';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+import { NEWS_API_URL } from '../ducks/news';
 
 import MockStoreProvider, { store } from '../__test-helpers/MockStore';
 
 import { render, screen } from '@testing-library/react';
 
 import NotificationMenu from './';
+
+const mockNewsItems = [{
+  title: 'howdy there',
+  description: 'do you want to earn a shiny new golden EarthRanger badge? Inquire within!',
+  link: 'https://earthranger.com/hello/wow',
+  read: false,
+}, {
+  title: 'howdy doody',
+  description: 'This message has already been read, but you are welcome to read it again pal',
+  link: 'https://earthranger.com/about',
+  read: true,
+}, {
+  title: '123 here we go',
+  description: 'having a toddler means a life sentence of listening to "baby shark" until your eyes fall out, and that\'s ok',
+  link: 'https://earthranger.com/yep/neat',
+  read: false,
+}];
+
+const server = setupServer(
+  rest.get(NEWS_API_URL, (req, res, ctx) => {
+    return res(ctx.json(mockNewsItems));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 it('renders without crashing', () => {
   const container = document.createElement('div');
--- src/ducks/news.js
+++ src/ducks/news.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import unionBy from 'lodash/unionBy';
 import { API_URL } from '../constants';
 
-const NEWS_API = `${API_URL}news/`;
+export const NEWS_API_URL = `${API_URL}news/`;
 const { get, post } = axios;
 
 const FETCH_NEWS_SUCCESS = 'FETCH_NEWS_SUCCESS';
@@ -10,7 +10,7 @@ const FETCH_NEWS_NEXT_PAGE_SUCCESS = 'FETCH_NEWS_NEXT_PAGE_SUCCESS';
 
 const READ_NEWS_ITEMS = 'READ_NEWS_ITEMS';
 
-export const fetchNews = (params = {}) => get(NEWS_API, { params: {
+export const fetchNews = (params = {}) => get(NEWS_API_URL, { params: {
   include_additional_data: false, page_size: 25, ...params,
 } });
 
@@ -20,7 +20,7 @@ export const readNews = (news) => {
     .reduce((string, id, index, array) =>
       `${string}${id}${array.length - 1 === index ? '' : ','}`
     , 'read=');
-  return post(`${NEWS_API}${paramString}`);
+  return post(`${NEWS_API_URL}${paramString}`);
 };
 
 export const readNewsSuccess = news => ({
