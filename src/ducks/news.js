5f3ef7fdbcfc9b07cab588d43015f5ae2dae835d
Joshua Krautwurst <joshuak@vulcan.com>
2021-07-23 09:49:13 -0700

    removing unused code

--- src/ducks/news.js
+++ src/ducks/news.js
@@ -1,14 +1,8 @@
 import axios from 'axios';
-import unionBy from 'lodash/unionBy';
 import { API_URL } from '../constants';
 
 export const NEWS_API_URL = `${API_URL}news`;
 const { get, post } = axios;
-
-const FETCH_NEWS_SUCCESS = 'FETCH_NEWS_SUCCESS';
-const FETCH_NEWS_NEXT_PAGE_SUCCESS = 'FETCH_NEWS_NEXT_PAGE_SUCCESS';
-
-const READ_NEWS_ITEMS = 'READ_NEWS_ITEMS';
 
 export const fetchNews = (params = {}) => get(NEWS_API_URL, { params: {
   page_size: 10, ...params,
@@ -22,39 +16,4 @@ export const readNews = (news) => {
     , '?read=');
 
   return post(`${NEWS_API_URL}${paramString}`);
-};
-
-export const readNewsSuccess = news => ({
-  type: READ_NEWS_ITEMS,
-  payload: news,
-});
-
-
-export const fetchNewsSuccess = news => ({
-  type: FETCH_NEWS_SUCCESS,
-  payload: news,
-});
-
-export const fetchNewsNextPageSuccess = nextPage => ({
-  type: FETCH_NEWS_NEXT_PAGE_SUCCESS,
-  payload: nextPage,
-});
-
-export default (state, action) => {
-  const { type, payload } = action;
-
-  if (type === FETCH_NEWS_SUCCESS) {
-    return payload;
-  }
-
-  if (type === FETCH_NEWS_NEXT_PAGE_SUCCESS ||
-  type === READ_NEWS_ITEMS) {
-    return {
-      ...state,
-      results: unionBy(state.results || [], payload.results, 'id'),
-    };
-  }
-
-  return state;
-  
 };
