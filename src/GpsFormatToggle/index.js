e343ce2e1d7b96b90dce3112c0081356411327b1
Joshua Krautwurst <38018017+JoshuaVulcan@users.noreply.github.com>
2021-08-18 13:23:04 -0700

    moving clipbboard/copy button into its own component for better re-use and separation of concerns

--- src/GpsFormatToggle/index.js
+++ src/GpsFormatToggle/index.js
@@ -1,13 +1,12 @@
-import React, { useCallback, useRef, useState, memo } from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Alert from 'react-bootstrap/Alert';
 
 import { updateUserPreferences } from '../ducks/user-preferences';
 import { calcGpsDisplayString, GPS_FORMATS } from '../utils/location';
 import { trackEvent } from '../utils/analytics';
 
-import { ReactComponent as ClipboardIcon } from '../common/images/icons/clipboard-icon.svg';
+import TextCopyBtn from '../TextCopyBtn';
 
 import styles from './styles.module.scss';
 
@@ -16,39 +15,14 @@ const gpsFormats = Object.values(GPS_FORMATS);
 const GpsFormatToggle = (props) => {
   const { updateUserPreferences, showGpsString = true, showCopyControl = showGpsString, lat, lng, currentFormat, className, ...rest } = props;
 
-  const [copySuccess, showCopySuccess] = useState(false);
-  const copySuccessMsgAlertTimeout = useRef(null);
-
   const onGpsFormatClick = (gpsFormat) => {
     trackEvent('GPS Format', 'Change GPS Format', `GPS Format:${gpsFormat}`);
     updateUserPreferences({
       gpsFormat,
     });
-    showCopySuccess(false);
   };
 
   const gpsString = showGpsString && calcGpsDisplayString(lat, lng, currentFormat) ? calcGpsDisplayString(lat, lng, currentFormat) : null;
-
-  const onCopySuccess = useCallback(() => {
-    clearTimeout(copySuccessMsgAlertTimeout.current);
-    showCopySuccess(true);
-
-    copySuccessMsgAlertTimeout.current = setTimeout(() => {
-      showCopySuccess(false);
-    }, [2500]);
-  }, []);
-
-  const onClickCopy = useCallback(async () => {
-    showCopySuccess(false);
-    
-    await navigator.clipboard.writeText(gpsString)
-      .catch((error) => {
-        console.warn('error copying value to clipboard', error);
-      });
-
-    onCopySuccess();
-  }, [gpsString, onCopySuccess]);
-
 
 
   return (
@@ -61,8 +35,7 @@ const GpsFormatToggle = (props) => {
       </ul> 
       {showGpsString && <div className={styles.gpsStringWrapper}>
         <span className={styles.value}>{gpsString}</span>
-        {showCopyControl && <ClipboardIcon onClick={onClickCopy} />}
-        {copySuccess && <Alert className={styles.copySuccessMsg} variant='success'>Copied to clipboard</Alert>}
+        {showCopyControl && <TextCopyBtn text={gpsString} />}
       </div>
       }
     </div>
