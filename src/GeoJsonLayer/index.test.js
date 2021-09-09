92dbe5754ed1b2f027e431275e504a9ab5063a2b
Joshua Krautwurst <joshuak@Joshuas-MacBook-Pro.local>
2021-09-09 12:02:06 -0700

    test scaffolding

---
+++ src/GeoJsonLayer/index.test.js
@@ -0,0 +1,33 @@
+import { oneOfType } from "prop-types";
+
+it('renders without crashing', () => {
+  
+});
+
+it('adds a source and layer based on the id provided', () => {
+
+});
+
+it('removes the source and layer on unmount', () => {
+
+});
+
+it('updates data', () => {
+
+});
+
+it('updates changed layout props', () => {
+
+});
+
+it('updates changed paint props', () => {
+
+});
+
+it('updates changed filter props', () => {
+
+});
+
+it('updates changed before props', () => {
+
+});
