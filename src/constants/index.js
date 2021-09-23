544a6fce796062eaffc099bf783567b5bb5ba6f3
Joshua Krautwurst <joshuak@Joshuas-MacBook-Pro.local>
2021-09-22 19:41:53 -0700

    style lint config. code style tweaks and improvements. toast color change to fit within our existing color palette.

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ import { INITIAL_FILTER_STATE } from '../ducks/event-filter';
 
 import layoutVariables from '../common/styles/_layout.scss';
 
-const { POSITION: TOAST_POSITIONS } = toast;
+const { POSITION: TOAST_POSITIONS, Slide } = toast;
 
 export const {
   REACT_APP_DAS_HOST,
@@ -246,6 +246,7 @@ export const GEOLOCATOR_OPTIONS = {
 
 export const DEFAULT_TOAST_CONFIG = {
   position: TOAST_POSITIONS.TOP_CENTER,
+  transition: Slide,
 };
 
 export const MAPBOX_STYLE_LAYER_SOURCE_TYPES = ['mapbox_style'];
