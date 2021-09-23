3e3065f4110b87e27f02824167d276c5d60dc26e
Joshua Krautwurst <joshuak@Joshuas-MacBook-Pro.local>
2021-09-23 09:36:47 -0700

    socket handler test setup

--- src/socket/handlers.test.js
+++ src/socket/handlers.test.js
@@ -1,3 +1,41 @@
-test('showing a warning toast when a new event does not match the current report filter', () => {
+import * as socketHandlers from './handlers';
+import * as toastUtils from '../utils/toast';
+import store from '../store';
 
+const { showFilterMismatchToastForHiddenReports } = socketHandlers;
+const { showErrorToast } = toastUtils;
+
+const MOCK_USER_ID = '12312-adsfsadf-e413-666';
+
+jest.mock('../store');
+
+beforeEach(() => {
+  jest.spyOn(socketHandlers, 'showFilterMismatchToastForHiddenReports');
+  jest.spyOn(toastUtils, 'showErrorToast');
+
+  store.getState.mockReturnValue({
+    data: {
+      user: {
+        id: MOCK_USER_ID,
+      }, eventTypes: [],
+    }
+  });
+});
+
+test('showing a warning toast when a new event does not match the current report filter', () => {
+  const msg = { event_data: {
+    title: 'howdy',
+    updates: [
+      {
+        time: new Date().toISOString(),
+        user: {
+          id: MOCK_USER_ID,
+        }
+      }
+    ]
+  }, event_id: '2-dasdf-23113', };
+
+  showFilterMismatchToastForHiddenReports(msg);
+
+  expect(showErrorToast).toHaveBeenCalled();
 });
