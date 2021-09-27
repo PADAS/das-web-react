c30590e9b61f7edbcac86bb4c90a0b58dfc263e5
Joshua Krautwurst <joshuak@Joshuas-MacBook-Pro.local>
2021-09-27 15:10:05 -0700

    updated filter button styles + linting

--- src/PatrolFilter/index.js
+++ src/PatrolFilter/index.js
@@ -24,6 +24,7 @@ import SearchBar from '../SearchBar';
 import { ReactComponent as ClockIcon } from '../common/images/icons/clock-icon.svg';
 
 import styles from '../EventFilter/styles.module.scss';
+import patrolFilterStyles from './styles.module.scss';
 
 /* const PATROL_STATUS_CHOICES = [
   { value: 'cancelled', 
@@ -45,7 +46,7 @@ import styles from '../EventFilter/styles.module.scss';
 ]; */
 
 const PatrolFilter = (props) => {
-  const { children, className, patrolFilter, /* reporters, */ resetGlobalDateRange, updatePatrolFilter } = props;
+  const { children, className = '', patrolFilter, /* reporters, */ resetGlobalDateRange, updatePatrolFilter } = props;
   const { /* status, */ filter: { date_range, /* patrol_type: currentFilterReportTypes, */ /* leader, */ text } } = patrolFilter;
 
   // const patrolTypeFilterEmpty = currentFilterReportTypes && !currentFilterReportTypes.length;
@@ -225,25 +226,23 @@ const PatrolFilter = (props) => {
   //   </Popover.Content>
   // </Popover>;
 
-  return <form className={`${styles.form} ${className}`} onSubmit={e => e.preventDefault()}>
-    <div className={styles.controls}  ref={containerRef}>
-      {/* <OverlayTrigger shouldUpdatePosition={true} rootClose trigger='click' placement='auto' overlay={FilterPopover} flip={true}>
+  return <div ref={containerRef} className={`${patrolFilterStyles.form} ${className}`} onSubmit={e => e.preventDefault()}>
+    {/* <OverlayTrigger shouldUpdatePosition={true} rootClose trigger='click' placement='auto' overlay={FilterPopover} flip={true}>
         <span className={`${styles.popoverTrigger} ${filterModified ? styles.modified : ''}`}>
           <FilterIcon className={styles.filterIcon} onClick={onPatrolFilterIconClicked} />
           <span>Filter</span>
         </span>
       </OverlayTrigger> */}
-      <OverlayTrigger shouldUpdatePosition={true} rootClose trigger='click' placement='auto' overlay={FilterDatePopover} flip={true}>
-        <span className={`${styles.popoverTrigger} ${dateRangeModified ? styles.modified : ''}`} onClick={onDateFilterIconClicked}>
-          <ClockIcon className={styles.clockIcon} />
-          <span>Dates</span>
-        </span>
-      </OverlayTrigger>
-      <SearchBar className={styles.search} placeholder='Search Patrols...' value={filterText}
+    <OverlayTrigger shouldUpdatePosition={true} rootClose trigger='click' placement='auto' overlay={FilterDatePopover} flip={true}>
+      <Button variant={dateRangeModified ? 'primary' : 'light'} size='sm' className={`${patrolFilterStyles.popoverTrigger} ${patrolFilterStyles.dateFilterButton}`} onClick={onDateFilterIconClicked}>
+        <ClockIcon className={styles.clockIcon} />
+        <span>Dates</span>
+      </Button>
+    </OverlayTrigger>
+    <SearchBar className={`${styles.search} ${patrolFilterStyles.search}`} placeholder='Search Patrols...' value={filterText}
         onChange={onSearchChange} onClear={onSearchClear} />
-      {children}
-    </div>
-  </form>;
+    {children}
+  </div>;
 };
 
 const mapStateToProps = (state) =>
