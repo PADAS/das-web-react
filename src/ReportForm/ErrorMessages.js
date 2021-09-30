72f6cf7fe211cc25339d9c6b10eb729653bfcacd
Patsy Iturbe Villegas <patsyiturbevillegas@Patsys-MacBook-Pro.local>
2021-09-30 10:47:50 -0500

    show report errors for schema validations

--- src/ReportForm/ErrorMessages.js
+++ src/ReportForm/ErrorMessages.js
@@ -2,16 +2,14 @@ import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import Alert from 'react-bootstrap/Alert';
 
-import { generateErrorListForApiResponseDetails } from '../utils/events';
-
 import styles from './styles.module.scss';
 
 const ReportFormErrorMessages = (props) => {
   const { errorData, onClose } = props;
   return <Alert onClose={onClose} dismissible={true} className={styles.saveErrorAlert} variant='danger'>
     <h5>Error saving report: {errorData.message}</h5>
-    {<ul>
-      {generateErrorListForApiResponseDetails(errorData).map(item =>
+    {errorData.length && <ul>
+      {errorData.map(item =>
         <li key={`${item.label} ${item.message}`}>
           <strong>{item.label}</strong>: <span>{item.message}</span>
         </li>)}
