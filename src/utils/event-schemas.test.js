4a581a49dc49bb28506d6c52d75cbc00de05c389
Joshua Krautwurst <joshuak@Joshuas-MacBook-Pro.local>
2021-10-26 10:54:13 -0500

    wording tweaks for style consistency

--- src/utils/event-schemas.test.js
+++ src/utils/event-schemas.test.js
@@ -3,11 +3,11 @@ import { getLinearErrorPropTree, filterOutErrorsForHiddenProperties } from './ev
 describe('getLinearErrorPropTree', () => {
   const errorPropTree = '.properties[\'reportcounty\'].enum';
 
-  it('extracts the field\'s property name from the error "property" prop tree', () => {
+  test('extracting the field\'s property name from the error "property" prop tree', () => {
     expect(getLinearErrorPropTree(errorPropTree)).toEqual(['reportcounty']);
   });
 
-  it('works for multi-dimensional objects', () => {
+  test('building the array for multi-dimensional objects', () => {
     const complexErrorPropTree = '.properties[\'reportcounty\'].properties[\'kittycat\'].properties[\'wowneat\'].enum';
 
     expect(getLinearErrorPropTree(complexErrorPropTree)).toEqual(['reportcounty', 'kittycat', 'wowneat']);
@@ -51,10 +51,10 @@ describe('filterOutErrorsForHiddenProperties', () => {
 
   const filtered = filterOutErrorsForHiddenProperties(errors, uiSchema);
 
-  it('keeps an error if its value is included in the ui:groups', () => {
+  test('keeping an error if its value is included in the ui:groups', () => {
     expect(filtered).toEqual(expect.arrayContaining([error2]));
   });
-  it('removes an error if its value is NOT included in the ui:groups', () => {
+  test('removing an error if its value is NOT included in the ui:groups', () => {
     expect(filtered).not.toEqual(expect.arrayContaining([error1]));
   });
 });
