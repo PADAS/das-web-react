40091c907da7b949184a7b6fa6203b7ff63f265f
Levy-Naibei <levynaibei2@gmail.com>
2021-10-27 10:44:40 +0300

    added query results filter

--- src/LocationSearchControl/index.js
+++ src/LocationSearchControl/index.js
@@ -23,6 +23,7 @@ const LocationSearch = (props) => {
   const [query, setQuery] = useState('');
   const [selectedLocation, setSelectedLocation] = useState(null); /* eslint-disable-line no-unused-vars */
   const [errors, setErrors] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const toggleActiveState = useCallback(() => setActiveState(!active), [active]);
@@ -72,8 +73,19 @@ const LocationSearch = (props) => {
     }
   }, [query]);
 
+  useEffect(() => {
+    if (query.length > 1) {
+      const matches = locations.filter(match => {
+        return match.placeName.toLowerCase().includes(query.toLocaleLowerCase());
+      });
+      console.log('suggestions', matches);
+      setSuggestions(matches);
+    }
+  }, [query, locations]);
+
   const handleClearSearch = () => {
     setQuery('');
+    setLocations([]);
   };
 
   const onKeyDown = (event) => {
@@ -87,6 +99,7 @@ const LocationSearch = (props) => {
         setQuery('');
       } else {
         setQuery('');
+        setLocations([]);
       }
     }
   };
@@ -137,7 +150,7 @@ const LocationSearch = (props) => {
     <p className={styles.zero_results} key={index}> {err.noResults} </p>
   ));
 
-  const querySuggestions = locations.map((location, index) => (
+  const querySuggestions = suggestions.map((location, index) => (
     <li
       className={styles.suggestion}
       key={index}
