eb5f21707599572f1fed50b910093df43be2ef92
Patsy <dev.patsy.iv@gmail.com>
2021-11-05 16:42:01 -0600

    add track event factory that works with specified categories

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,6 +1,7 @@
 import debounce from 'lodash/debounce';
 
 import { CLIENT_BUILD_VERSION } from '../constants';
+
 /**
  * ReactGA convenience functions.
  * 
@@ -8,6 +9,16 @@ import { CLIENT_BUILD_VERSION } from '../constants';
  * that rely on the ReactGA library.
  */
 import ReactGA from 'react-ga';
+
+export const EVENT_FILTER_CATEGORY = 'Event Filter';
+export const MAP_INTERACTION_CATEGORY = 'Map Interaction';
+export const MAP_LAYERS_CATEGORY = 'Map Layers';
+export const BASE_LAYER_CATEGORY = 'Base Layer';
+export const PATROL_CARD_CATEGORY = 'Patrol Card';
+export const PATROL_MODAL_CATEGORY = 'Patrol Modal';
+export const PATROL_FILTER_CATEGORY = 'Patrol Filter';
+export const REPORT_EXPORT_CATEGORY = 'Report Export';
+export const REPORTS_CATEGORY = 'Reports';
 
 /**
  * Function to emit a GA event.
@@ -24,7 +35,19 @@ export function trackEvent(category, action, label=null) {
   });
 };
 
-export const debouncedTrackEvent = (delay = 300) => debounce(trackEvent, delay);
+
+/**
+ * Function to create a tracker for provided category
+ * @param {string} category 
+ */
+export function trackEventFactory (category) {
+  const track = (action, label) => trackEvent(category, action, label);
+
+  return {
+    track,
+    debouncedTrack: (delay = 300) => debounce(track, delay)
+  };
+};
 
 /** 
  * Function to set the user role as a GA custom dimension.
