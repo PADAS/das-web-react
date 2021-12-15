ffeaa14aad48d42978e86cf331fc2d8a0c28175d
Joshua Krautwurst <joshuak@Joshuas-MacBook-Pro.local>
2021-12-15 15:26:12 -0800

    persistent volume! integration into the docker-compose! env-driven config! lookin good.

--- das-web-proxy/index.js
+++ das-web-proxy/index.js
@@ -1,18 +1,39 @@
 import express from 'express';
 import http from 'http';
 import { PORT } from './constants.js';
-
+import { MongoClient } from 'mongodb';
 import { fetchCurrentUserForRequest } from './user/index.js';
 
-const app = express();
-const server = http.createServer(app);
+try {
 
-app.use('/preferences*', [fetchCurrentUserForRequest]);
 
-app.get('/preferences/:userId', (req, res) => {
-  res.send(res.userData);
-});
+  const serviceUrl = 'mongodb://pref-db';
 
-console.log('listening on port', PORT);
 
-server.listen(PORT);
+  const app = express();
+  const server = http.createServer(app);
+
+  MongoClient.connect(serviceUrl, (err, db) => {
+    if (!err) {
+      console.log('connected', db);
+    }
+  });
+
+  app.use('/preferences*', [fetchCurrentUserForRequest]);
+
+  app.get('/preferences/:userId', (req, res) => {
+    const userId = res?.userData?.id;
+
+    if (!userId) {
+      res.sendStatus(404);
+    }
+
+    res.status(200).send(res.userData);
+  });
+
+  console.log('listening on port', PORT);
+
+  server.listen(PORT);
+} catch (e) {
+  console.warn('ERROR ON SERVER', e);
+}
