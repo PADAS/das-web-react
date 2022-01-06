2c22399da1d5302f3502e5ce4866d754ba5be035
Luis Alfonso Chávez Abbadie <luixlive@192.168.1.5>
2022-01-06 11:21:38 -0600

    DAS-7556: Add navigation tabs

--- src/PatrolDrawer/index.js
+++ src/PatrolDrawer/index.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Drawer from 'react-modern-drawer';
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
 
 import { DrawersContext, patrolDrawerId } from '../DrawersLayer';
 
@@ -8,33 +10,61 @@ import styles from './styles.module.scss';
 
 import 'react-modern-drawer/dist/index.css';
 
+const NAVIGATION_PLAN_EVENT_KEY = 'plan';
+const NAVIGATION_TIMELINE_EVENT_KEY = 'timeline';
+const NAVIGATION_HISTORY_EVENT_KEY = 'history';
+
 const PatrolDrawer = () => {
   const { drawers, hideDrawer } = useContext(DrawersContext);
   const { data, isOpen } = drawers[patrolDrawerId];
-
-  // TODO: Usar https://react-bootstrap.github.io/components/tabs/#custom-tab-layout para las tabs
 
   return <Drawer className="drawer" open={isOpen} onClose={() => hideDrawer(patrolDrawerId)} direction='right'>
     <div className={styles.header}>
       Vehicle Patrol
     </div>
 
-    <div className={styles.body}>
-      <div className={styles.navigation}></div>
+    <Tab.Container defaultActiveKey={NAVIGATION_PLAN_EVENT_KEY}>
+      <div className={styles.body}>
+        <Nav className={styles.navigation}>
+          <Nav.Item>
+            <Nav.Link eventKey={NAVIGATION_PLAN_EVENT_KEY}>Plan</Nav.Link>
+          </Nav.Item>
 
-      <div className={styles.content}></div>
+          <Nav.Item>
+            <Nav.Link eventKey={NAVIGATION_TIMELINE_EVENT_KEY}>Timeline</Nav.Link>
+          </Nav.Item>
 
-      <div className={styles.footer}>
-        <Button
-          className={styles.exitButton}
-          onClick={() => hideDrawer(patrolDrawerId)}
-          type="button"
-          variant="secondary"
-        >
-          Exit
-        </Button>
+          <Nav.Item>
+            <Nav.Link eventKey={NAVIGATION_HISTORY_EVENT_KEY}>History</Nav.Link>
+          </Nav.Item>
+        </Nav>
+
+        <Tab.Content className={styles.content}>
+          <Tab.Pane eventKey={NAVIGATION_PLAN_EVENT_KEY}>
+            Plan
+          </Tab.Pane>
+
+          <Tab.Pane eventKey={NAVIGATION_TIMELINE_EVENT_KEY}>
+            Timeline
+          </Tab.Pane>
+
+          <Tab.Pane eventKey={NAVIGATION_HISTORY_EVENT_KEY}>
+            History
+          </Tab.Pane>
+        </Tab.Content>
+
+        <div className={styles.footer}>
+          <Button
+            className={styles.exitButton}
+            onClick={() => hideDrawer(patrolDrawerId)}
+            type="button"
+            variant="secondary"
+          >
+            Exit
+          </Button>
+        </div>
       </div>
-    </div>
+    </Tab.Container>
   </Drawer>;
 };
 
