f4b72264624a7ec322c2b55df7ee588f15c3f427
Patsy <dev.patsy.iv@gmail.com>
2022-01-17 19:06:24 -0600

    show default prop for static sensors in map layers

--- src/SubjectGroupList/SubjectListItem.js
+++ src/SubjectGroupList/SubjectListItem.js
@@ -7,12 +7,18 @@ import listStyles from '../SideBar/styles.module.scss';
 
 const SubjectListItem = (props) => { // eslint-disable-line react/display-name
 
-  const subjectRadioImage = useMemo(() => isRadioWithImage(props), [props]);
-
   const { map, ...rest } = props;
+  const subjectRadioImage = useMemo(() => isRadioWithImage(props), [props]);
+  const isStaticTypeObject = props.is_static;
+  const defaultProperty = props?.device_status_properties?.find(deviceProperty => deviceProperty?.default ?? false) ?? '';
+
   return <Fragment>
-    <span className={listStyles.itemTitle}>{subjectRadioImage && <img src={subjectRadioImage} alt={props.name} />} {props.name}</span>
-    <SubjectControls showLabels={false} className={listStyles.controls} map={map} showTitles={false} subject={rest} />
+    <p className={listStyles.itemTitle}>
+      {subjectRadioImage && <img src={subjectRadioImage} alt={props.name} />}
+      {props.name}
+      {isStaticTypeObject && <span>{` ${defaultProperty.label}: ${defaultProperty.value} ${defaultProperty.units}`}</span>}
+    </p>
+    <SubjectControls showLabels={false} className={listStyles.controls} map={map} showTitles={false} subject={rest} showTrackButton={!isStaticTypeObject} showHeatmapButton={!isStaticTypeObject}/>
   </Fragment>;
 };
 
