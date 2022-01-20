96f5ff04f24db6e1e75f6582b7ced29bd3daa299
Patsy <dev.patsy.iv@gmail.com>
2022-01-20 11:23:30 -0600

    show featured attribute also for mobile subjects

--- src/SubjectGroupList/SubjectListItem.js
+++ src/SubjectGroupList/SubjectListItem.js
@@ -1,5 +1,6 @@
 import React, { memo, useMemo, Fragment } from 'react';
 import SubjectControls from '../SubjectControls';
+import isEmpty from 'lodash/isEmpty';
 
 import { isRadioWithImage, isStaticTypeSensor, getSubjectDefaultDeviceProperty } from '../utils/subjects';
 
@@ -15,8 +16,8 @@ const SubjectListItem = (props) => {
   return <Fragment>
     <p className={listStyles.itemTitle} data-testid='subject-item-name'>
       {subjectRadioImage && <img src={subjectRadioImage} alt={subject.name} />}
-      {subject.name}
-      {isStaticTypeObject && <span>{`${defaultProperty.label}: ${defaultProperty.value} ${defaultProperty.units}`}</span>}
+      <span> {subject.name} </span>
+      {!isEmpty(defaultProperty) && <span className={listStyles.defaultProperty}>{`${defaultProperty.label}: ${defaultProperty.value} ${defaultProperty.units}`}</span>}
     </p>
     <SubjectControls showLabels={false} className={listStyles.controls} map={map} showTitles={false} subject={subject} showTrackButton={!isStaticTypeObject} showHeatmapButton={!isStaticTypeObject}/>
   </Fragment>;
