0fa18a18cc485b14c7aeca3c6858b44541578545
Patsy <dev.patsy.iv@gmail.com>
2022-01-20 11:25:48 -0600

    change test to match with new implementation

--- src/SubjectGroupList/SubjectListItem.test.js
+++ src/SubjectGroupList/SubjectListItem.test.js
@@ -33,42 +33,38 @@ test('rendering without crashing', () => {
   </Provider>);
 });
 
-describe('Rendering static sensor items', () => {
+describe('Rendering subject list item', () => {
   beforeEach(async () => {
     map = createMapMock();
+  });
+
+  test('it should show the subject name followed with the feature default property if the subject has one', async () => {
     render(<Provider store={mockStore(store)}>
       <MapContext.Provider value={map}>
         <SubjectListItem map={map} {...staticSubjectFeature.properties}/>
       </MapContext.Provider>
     </Provider>);
-  });
 
-  test('it should show the subject name followed with the feature default property', async () => {
     const subjectTitle = screen.getByTestId('subject-item-name');
     const defaultSubjectProperty = getSubjectDefaultDeviceProperty(staticSubjectFeature);
 
     const subjectName = staticSubjectFeature.properties.name;
     const defaultSubjectPropertyLabel = `${defaultSubjectProperty.label}: ${defaultSubjectProperty.value} ${defaultSubjectProperty.units}`;
 
-    expect(subjectTitle.textContent).toEqual(`${subjectName}${defaultSubjectPropertyLabel}`);
+    expect(subjectTitle.textContent).toEqual(` ${subjectName} ${defaultSubjectPropertyLabel}`);
   });
-});
 
-describe('Rendering mobile subject items', () => {
-  beforeEach(async () => {
-    map = createMapMock();
+  test('it should not show the feature default property if the subject does not have one', async () => {
     render(<Provider store={mockStore(store)}>
       <MapContext.Provider value={map}>
         <SubjectListItem map={map} {...subjectFeatureWithOneDeviceProp.properties}/>
       </MapContext.Provider>
     </Provider>);
-  });
 
-  test('it should show the subject name without a feature default property', async () => {
     const subjectName = screen.getByTestId('subject-item-name');
     const defaultSubjectProperty = getSubjectDefaultDeviceProperty(subjectFeatureWithOneDeviceProp);
 
-    expect(subjectName.textContent).toEqual(subjectFeatureWithOneDeviceProp.properties.name);
+    expect(subjectName.textContent).toEqual(` ${subjectFeatureWithOneDeviceProp.properties.name} `);
     expect(defaultSubjectProperty).toMatchObject({});
   });
 });
