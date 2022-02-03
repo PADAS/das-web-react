1cea56b1b97489fc49826384856fdc4472c59a18
Ludwig <luixlive@gmail.com>
2022-02-03 11:20:13 -0600

    DAS-7317: Remove events from clusters when removing map layer

--- src/selectors/clusters.js
+++ src/selectors/clusters.js
@@ -2,10 +2,12 @@ import { createSelector } from 'reselect';
 
 import { getTimeSliderState } from './';
 
+const getShowReportsOnMap = ({ view: { showReportsOnMap } }) => showReportsOnMap;
+
 // This is the place to query a redux configuration flag to cluster / uncluster things
 export const getShouldEventsBeClustered = createSelector(
-  [getTimeSliderState],
-  (timeSliderState) => !timeSliderState.active
+  [getShowReportsOnMap, getTimeSliderState],
+  (showReportsOnMap, timeSliderState) => !!showReportsOnMap && !timeSliderState.active
 );
 
 export const getShouldSubjectsBeClustered = createSelector(
