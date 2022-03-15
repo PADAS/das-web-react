7e5d46ee7bcdb17e516eb419f32a01c04097f44b
Ludwig <luixlive@gmail.com>
2022-03-15 17:07:02 -0600

    DAS-7768: Specify timezone in date test

--- src/FriendlyFilterString/index.test.js
+++ src/FriendlyFilterString/index.test.js
@@ -6,7 +6,7 @@ import { EVENT_SORT_OPTIONS, SORT_DIRECTION } from '../utils/event-filter';
 
 describe('FriendlyFilterString', () => {
   beforeAll(() => {
-    jest.useFakeTimers('modern').setSystemTime(new Date('February 20 2022 12:30').getTime());
+    jest.useFakeTimers('modern').setSystemTime(new Date('February 20 2022 12:30 GMT-0600').getTime());
   });
 
   test('prints a friendly string for a non-filtered case', async () => {
