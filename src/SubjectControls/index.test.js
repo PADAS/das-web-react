6d760f71b5e66ca8aa34eb3d0102fbac81c55277
Joshua Krautwurst <joshuak@Joshuas-MacBook-Pro.local>
2022-05-12 09:08:35 -0700

    not showing historical data controls in map layer list items. fixing interval issues with 'pingSocket'

--- src/SubjectControls/index.test.js
+++ src/SubjectControls/index.test.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import { render, screen, userEvent, waitFor } from '@testing-library/react';
 
+import mockSubjectData from '../__test-helpers/fixtures/subjects';
+
+import { Provider } from 'react-redux';
+import { mockStore } from '../__test-helpers/MockStore';
+
 import SubjectControls from './';
 
-test('rendering without crashing', () => {
-  render(<SubjectControls />);
-});
 
-describe('the track button', () => {
 
-});
+describe('SubjectControls', () => {
+  let store;
+  const [subject] = mockSubjectData;
+  const { properties } = subject;
 
-describe('the heatmap button', () => {
+  beforeEach(() => {
+    store = mockStore({
+      data: {
+        tracks: {},
+      },
+      view: {
+        subjectTrackState: { pinned: [], visible: [] },
+        heatmapSubjectIDs: [],
+      },
+    });
+  });
 
-});
+  test('rendering without crashing', () => {
+    render(
+      <Provider store={store}>
+        <SubjectControls subject={properties} />
+      </Provider>);
+  });
 
-describe('the "jump to location" button', () => {
+  test('showing the historical data button', () => {
+    render(
+      <Provider store={store}>
+        <SubjectControls subject={properties} showHistoryButton={true} />
+      </Provider>
+    );
 
-});
-
-describe('the "historical data" button', () => {
-
+  });
 });
