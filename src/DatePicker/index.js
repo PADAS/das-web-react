213fe2c2e6a38aabbf62e74f0b620c388644077a
Patsy <dev.patsy.iv@gmail.com>
2022-05-12 09:17:05 -0500

    add custom header

--- src/DatePicker/index.js
+++ src/DatePicker/index.js
@@ -1,10 +1,14 @@
 import React, { useState, forwardRef } from 'react';
 import DatePicker from 'react-datepicker';
 import noop from 'lodash/noop';
+import getMonth from 'date-fns/get_month';
+import getYear from 'date-fns/get_year';
 
 import { ReactComponent as CalendarIcon } from '../common/images/icons/calendar.svg';
 import { ReactComponent as ArrowDown } from '../common/images/icons/arrow-down-small.svg';
 import { ReactComponent as ArrowUp } from '../common/images/icons/arrow-up-small.svg';
+import { ReactComponent as ChevronLeft } from '../common/images/icons/chevron-left.svg';
+import { ReactComponent as ChevronRight } from '../common/images/icons/chevron-right.svg';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import styles from './styles.module.scss';
@@ -46,15 +50,42 @@ const StyledDatePicker = ({ value,
       onCalendarOpen={handleOpen}
       onCalendarClose={handleClose}
       customInput={ !disableCustomInput ?
-        customInput || <CustomDefaultInput
-        value={value}
-        onClick={onChange}
-        className={className}
-        placeholderText={placeholderText}
-        isPopperOpen={isOpen}
-        /> : null}
+      customInput || <CustomDefaultInput
+      value={value}
+      onClick={onChange}
+      className={className}
+      placeholderText={placeholderText}
+      isPopperOpen={isOpen}
+      /> : null}
       showMonthDropdown
       showYearDropdown
+      renderCustomHeader={({
+        date,
+        changeYear,
+        changeMonth,
+        decreaseYear,
+        increaseYear,
+        decreaseMonth,
+        increaseMonth,
+        prevMonthButtonDisabled,
+        nextMonthButtonDisabled,
+      }) => (
+        <div className={styles.customHeader}>
+          <button onClick={decreaseYear}> <ChevronLeft/><ChevronLeft/> </button>
+          <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}> <ChevronLeft/> </button>
+
+          <DatePicker
+            selected={date}
+            dateFormat="yyyy"
+            showMonthYearPicker
+            onChange={(date) => { changeMonth(getMonth(date)); changeYear(getYear(date)); }}
+            customInput={<button className={styles.headerTitle} onClick={increaseMonth}> {`${date.toLocaleString('en-US', { month: 'short' })} ${getYear(date)}`} </button>}
+          />
+
+          <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}> <ChevronRight/> </button>
+          <button onClick={increaseYear}> <ChevronRight/><ChevronRight/> </button>
+        </div>
+      )}
       {...rest}
       >
       {children}
@@ -91,5 +122,6 @@ const CustomDefaultInput = forwardRef(({
     </button>
   </>;
 });
+
 
 export default forwardRef(StyledDatePicker);
