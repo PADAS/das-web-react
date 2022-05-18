a9a01d38bd781c2b28f34fe61c780fc77358ff6a
Ludwig <luixlive@gmail.com>
2022-05-18 10:00:18 -0500

    DAS-8077: Add event type label if the title is not the type

--- src/ReportDetailView/Header/index.js
+++ src/ReportDetailView/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import { PRIORITY_COLOR_MAP } from '../../utils/events';
@@ -15,8 +15,15 @@ const Header = ({ report, setTitle }) => {
   const titleInput = useRef();
 
   const isNewReport = !report.id;
+  const showEventType = report.title !== eventTypeTitle;
 
   const priorityTheme = PRIORITY_COLOR_MAP[report.priority];
+
+  useEffect(() => {
+    if (!report.title) {
+      setTitle(displayTitle);
+    }
+  }, [displayTitle, report.title, setTitle]);
 
   const onTitleBlur = useCallback((event) => {
     if (!event.target.textContent) {
@@ -33,15 +40,24 @@ const Header = ({ report, setTitle }) => {
 
     <p className={styles.serialNumber}>{report.serial_number}</p>
 
-    <div
-      className={styles.title}
-      contentEditable={true}
-      data-testid="reportDetailView-header-title"
-      onBlur={onTitleBlur}
-      ref={titleInput}
-      suppressContentEditableWarning
-    >
-      {displayTitle}
+    <div className={styles.titleAndType}>
+      {report.title && <div
+        className={styles.title}
+        contentEditable={true}
+        data-testid="reportDetailView-header-title"
+        onBlur={onTitleBlur}
+        ref={titleInput}
+        suppressContentEditableWarning
+      >
+        {report.title}
+      </div>}
+
+      {showEventType && <label
+        className={styles.eventType}
+        data-testid="reportDetailView-header-eventType"
+      >
+        {eventTypeTitle}
+      </label>}
     </div>
 
     {!isNewReport && <div className={styles.priorityAndDate}>
--- src/ReportDetailView/Header/index.test.js
+++ src/ReportDetailView/Header/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { eventTypes } from '../../__test-helpers/fixtures/event-types';
@@ -11,35 +11,58 @@ import { report } from '../../__test-helpers/fixtures/reports';
 
 describe('Header', () => {
   const setTitle = jest.fn();
-  beforeEach(() => {
-    render(
-      <Provider store={mockStore({ data: { eventTypes, patrolTypes } })}>
-        <Header report={report} setTitle={setTitle} />
-      </Provider>
-    );
-  });
-
   afterEach(() => {
     jest.restoreAllMocks();
   });
 
   test('renders correctly case of a 300 priority report', async () => {
+    render(
+      <Provider store={mockStore({ data: { eventTypes, patrolTypes } })}>
+        <Header report={report} setTitle={setTitle} />
+      </Provider>
+    );
+
     expect((await screen.findByTestId('reportDetailHeader-icon'))).toHaveClass('priority-300');
   });
 
-  test('triggers setTitle callback when the contenteditable loses focus', async () => {
+  test('sets the display title as the title if it was empty', async () => {
     expect(setTitle).toHaveBeenCalledTimes(0);
+
+    render(
+      <Provider store={mockStore({ data: { eventTypes, patrolTypes } })}>
+        <Header report={report} setTitle={setTitle} />
+      </Provider>
+    );
+
+    expect(setTitle).toHaveBeenCalledTimes(1);
+    expect(setTitle).toHaveBeenCalledWith('Light');
+  });
+
+  test('triggers setTitle callback when the contenteditable loses focus', async () => {
+    report.title = 'Light';
+    render(
+      <Provider store={mockStore({ data: { eventTypes, patrolTypes } })}>
+        <Header report={report} setTitle={setTitle} />
+      </Provider>
+    );
 
     const titleTextBox = await screen.findByTestId('reportDetailView-header-title');
     userEvent.type(titleTextBox, '2');
     userEvent.tab();
 
-    expect(setTitle).toHaveBeenCalledTimes(1);
-    expect(setTitle).toHaveBeenCalledWith('Light2');
+    await waitFor(() => {
+      expect(setTitle).toHaveBeenCalledTimes(1);
+      expect(setTitle).toHaveBeenCalledWith('Light2');
+    });
   });
 
   test('sets the event type title if user leaves the title input empty', async () => {
-    expect(setTitle).toHaveBeenCalledTimes(0);
+    report.title = 'Light';
+    render(
+      <Provider store={mockStore({ data: { eventTypes, patrolTypes } })}>
+        <Header report={report} setTitle={setTitle} />
+      </Provider>
+    );
 
     const titleTextBox = await screen.findByTestId('reportDetailView-header-title');
     userEvent.type(titleTextBox, '{backspace}{backspace}{backspace}{backspace}{backspace}');
@@ -47,5 +70,29 @@ describe('Header', () => {
 
     expect(setTitle).toHaveBeenCalledTimes(1);
     expect(setTitle).toHaveBeenCalledWith('Light');
+  });
+
+  test('shows the event type label if the title does not match the event type title', async () => {
+    report.title = 'Report!';
+    render(
+      <Provider store={mockStore({ data: { eventTypes, patrolTypes } })}>
+        <Header report={report} setTitle={setTitle} />
+      </Provider>
+    );
+
+    const eventTypeLabel = await screen.findByTestId('reportDetailView-header-eventType');
+
+    expect(eventTypeLabel).toHaveTextContent('Light');
+  });
+
+  test('doest not show the event type label if the title matches the event type title', async () => {
+    report.title = 'Light';
+    render(
+      <Provider store={mockStore({ data: { eventTypes, patrolTypes } })}>
+        <Header report={report} setTitle={setTitle} />
+      </Provider>
+    );
+
+    expect((await screen.queryByTestId('reportDetailView-header-eventType'))).toBeNull();
   });
 });
