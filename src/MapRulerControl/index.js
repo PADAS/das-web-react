d2b315dd797c4bac3d23fdc9bbef6abb0c40ec2c
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2022-08-05 15:25:11 -0700

    breaking up combined component code. test files.

--- src/MapRulerControl/index.js
+++ src/MapRulerControl/index.js
@@ -1,22 +1,16 @@
-import React, { memo, useState, useEffect, Fragment, useCallback, useMemo } from 'react';
-import { Popup } from 'react-mapbox-gl';
+import React, { memo, useState, useEffect, Fragment, useCallback } from 'react';
 import { connect } from 'react-redux';
 import Button from 'react-bootstrap/Button';
-import length from '@turf/length';
-import { lineString } from '@turf/helpers';
 import isEqual from 'react-fast-compare';
 
-import { calculatePopoverPlacement } from '../utils/map';
 import { withMap } from '../EarthRangerMap';
 import { ReactComponent as RulerIcon } from '../common/images/icons/ruler-icon.svg';
 import { trackEventFactory, MAP_INTERACTION_CATEGORY } from '../utils/analytics';
 
 import { useMapEventBinding } from '../hooks';
 
-import GpsFormatToggle from '../GpsFormatToggle';
-import AddReport from '../AddReport';
+import PointPopup from './PointPopup';
 import { setPickingMapLocationState } from '../ducks/map-ui';
-import { calcPositiveBearing } from '../utils/location';
 
 import MapDrawingTools, { DRAWING_MODES } from '../MapDrawingTools';
 import { LAYER_IDS } from '../MapDrawingTools/MapLayers';
@@ -188,8 +182,8 @@ const MapRulerControl = (props) => {
       </button>
     </div>
     {active && points.length > 1 && <>
-      {drawing && <MemoizedPointPopup map={map} points={points} pointIndex={points.length - 1} drawing={drawing} onClickFinish={onFinish} />}
-      {!drawing && popupPointSelected && <MemoizedPointPopup map={map} points={points} pointIndex={selectedPointIndex} drawing={drawing} />}
+      {drawing && <PointPopup map={map} points={points} pointIndex={points.length - 1} drawing={drawing} onClickFinish={onFinish} />}
+      {!drawing && popupPointSelected && <PointPopup map={map} points={points} pointIndex={selectedPointIndex} drawing={drawing} />}
     </>}
     {active && <MapDrawingTools drawing={drawing} drawingMode={DRAWING_MODES.POLYGON} points={points} onChange={onDrawChange} onClickPoint={onClickPoint} />}
   </>;
@@ -198,79 +192,3 @@ const MapRulerControl = (props) => {
 export default connect(null, { setPickingMapLocationState })(memo(withMap(MapRulerControl)));
 
 
-const PointPopup = (props) => {
-  const { drawing, map, onClickFinish, pointIndex, points } = props;
-  const isFirstPoint = pointIndex === 0;
-  const point = points[pointIndex];
-  const popupOffset = [0, -4];
-  const popupAnchorPosition = 'bottom';
-
-
-
-  const distanceFromStart = useMemo(() => {
-    if (isFirstPoint) return null;
-
-    const clonedPoints = [...points];
-    clonedPoints.length = (pointIndex + 1);
-
-    if (!clonedPoints.length) return null;
-
-    return `${length(lineString(clonedPoints)).toFixed(2)}km`;
-
-  }, [isFirstPoint, pointIndex, points]);
-
-  const bearingFromPrev = useMemo(() => {
-    if (isFirstPoint) return null;
-
-    const prevPoint = points[pointIndex - 1];
-
-    if (!prevPoint || !point) return null;
-
-    return calcPositiveBearing(prevPoint, point).toFixed(2);
-  }, [isFirstPoint, point, pointIndex, points]);
-
-  const [popoverPlacement, setPopoverPlacement] = useState('auto');
-
-  useEffect(() => {
-    const updatePopoverPlacement = async () => {
-      const updatedPopoverPlacement = await calculatePopoverPlacement(map, { lat: point[1], lng: point[0] });
-      setPopoverPlacement(updatedPopoverPlacement);
-    };
-
-    updatePopoverPlacement();
-  }, [map, point]);
-
-  if (!point) return null;
-
-  return <Popup className={`${styles.popup} ${drawing ? styles.unfinished : ''}`} offset={popupOffset} coordinates={point} anchor={popupAnchorPosition}>
-
-    {!drawing && <Fragment>
-      <GpsFormatToggle lng={point[0]} lat={point[1]} />
-      {points.length > 1 && !isFirstPoint && <Fragment>
-        <p>
-          <strong>Bearing:</strong> {bearingFromPrev}&deg; <br />
-          <strong>Distance from start:</strong> {distanceFromStart}
-        </p>
-      </Fragment>}
-      <AddReport
-        analyticsMetadata={{
-          category: MAP_INTERACTION_CATEGORY,
-          location: 'map ruler popup',
-        }}
-        reportData={{
-          location: {
-            latitude: point[1],
-            longitude: point[0],
-          }
-        }}
-        popoverPlacement={popoverPlacement}
-      />
-    </Fragment>}
-    {
-      drawing && <p onClick={onClickFinish} className={styles.finishButton}>
-        <RulerIcon />
-        click here to finish
-      </p>}
-  </Popup>;
-};
-const MemoizedPointPopup = memo(PointPopup);
