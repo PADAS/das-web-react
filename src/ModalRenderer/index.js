e03bd1dcac60aae698ceee5703b0fa07478f758e
Ludwig <luixlive@gmail.com>
2022-08-12 16:29:38 -0500

    ERA-7701: Close drawing mode when pressing escape

--- src/ModalRenderer/index.js
+++ src/ModalRenderer/index.js
@@ -1,48 +1,65 @@
 import React, { memo, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-bootstrap/Modal';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ImageModal from '../ImageModal';
 import { removeModal } from '../ducks/modals';
 
 import styles from './styles.module.scss';
 
-const ModalRenderer = ({ canShowModals, map, modals, removeModal }) => !!modals.length && <div
-  className={styles.modalBackdrop}
-  data-testid='modalsRenderer-container'
-  >
-  {/* Suspense for lazy loaded modals (avoid the page view suspense to handle them making the whole page blink) */}
-  <Suspense fallback={null}>
-    {modals.map((item) => {
-      const { content: ContentComponent, backdrop = 'static', forceShowModal = false, id, modalProps, ...rest } = item;
-      const showModal = forceShowModal || canShowModals;
+const ModalRenderer = ({ map }) => {
+  const dispatch = useDispatch();
 
-      return !!ContentComponent && <Modal
-        backdrop={backdrop}
-        backdropClassName={showModal ? styles.show : styles.hide}
-        centered
-        dialogClassName={showModal ? styles.show : styles.hide}
-        enforceFocus={false}
-        key={id}
-        show
-        style={{
-          display: showModal ? 'block' : 'none',
-          opacity: showModal ? '1' : '0',
-          transition: 'opacity 0.3s linear, display 0 linear 0.3s,'
-        }}
-        {...modalProps}
-        onHide={() => removeModal(id)}
-        {...(ContentComponent.type?.name === ImageModal.type.name
-          ? { className: `${modalProps?.className || ''} ${styles.modalImageBackground}` }
-          : {}
-        )}
-      >
-        <ContentComponent id={id} {...rest} map={map} />
-      </Modal>;
-    })}
-  </Suspense>
-</div>;
+  const canShowModals = useSelector((state) => state.view.modals.canShowModals);
+  const isPickingArea = useSelector((state) => state.view.mapLocationSelection.isPickingArea);
+  const isPickingLocation = useSelector((state) => state.view.mapLocationSelection.isPickingLocation);
+  const modals = useSelector((state) => state.view.modals.modals);
+
+  const pickingAreaOrLocationOnMap = isPickingLocation || isPickingArea;
+
+  return !!modals.length && <div
+    className={styles.modalBackdrop}
+    data-testid='modalsRenderer-container'
+    >
+    {/* Suspense for lazy loaded modals (avoid the page view suspense to handle them making the whole page blink) */}
+    <Suspense fallback={null}>
+      {modals.map((item) => {
+        const { content: ContentComponent, backdrop = 'static', forceShowModal = false, id, modalProps, ...rest } = item;
+        const showModal = forceShowModal || canShowModals;
+
+        const onHideModal = () => {
+          if (!pickingAreaOrLocationOnMap) {
+            dispatch(removeModal(id));
+          }
+        };
+
+        return !!ContentComponent && <Modal
+          backdrop={backdrop}
+          backdropClassName={showModal ? styles.show : styles.hide}
+          centered
+          dialogClassName={showModal ? styles.show : styles.hide}
+          enforceFocus={false}
+          key={id}
+          show
+          style={{
+            display: showModal ? 'block' : 'none',
+            opacity: showModal ? '1' : '0',
+            transition: 'opacity 0.3s linear, display 0 linear 0.3s,'
+          }}
+          {...modalProps}
+          onHide={onHideModal}
+          {...(ContentComponent.type?.name === ImageModal.type.name
+            ? { className: `${modalProps?.className || ''} ${styles.modalImageBackground}` }
+            : {}
+          )}
+        >
+          <ContentComponent id={id} {...rest} map={map} />
+        </Modal>;
+      })}
+    </Suspense>
+  </div>;
+};
 
 ModalRenderer.propTypes = {
   modals: PropTypes.arrayOf(PropTypes.shape({
@@ -53,7 +70,4 @@ ModalRenderer.propTypes = {
   })).isRequired,
 };
 
-const mapStateToProps = ({ view: { modals: { modals, canShowModals } } }) =>
-  ({ modals, canShowModals });
-
-export default connect(mapStateToProps, { removeModal })(memo(ModalRenderer));
+export default memo(ModalRenderer);
--- src/ReportGeometryDrawer/index.js
+++ src/ReportGeometryDrawer/index.js
@@ -1,14 +1,30 @@
-import React, { memo, useCallback } from 'react';
+import React, { memo, useCallback, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { setMapInteractionIsPickingArea } from '../ducks/map-ui';
 
 import Footer from './Footer';
 import ReportOverview from './ReportOverview';
 
 const ReportGeometryDrawer = () => {
+  const dispatch = useDispatch();
   // TODO: Add drawing mechanism here
 
   const onSaveGeometry = useCallback(() => {
 
   }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Escape') {
+        dispatch(setMapInteractionIsPickingArea(false));
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  }, [dispatch]);
 
   return <>
     <ReportOverview />
