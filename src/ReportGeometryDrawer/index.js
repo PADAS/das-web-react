bba43cf2736a1397a087f8329284b3e753837e9c
Ludwig <luixlive@gmail.com>
2022-09-02 12:03:34 -0500

    ERA-7664: Close the polygon if user clicks the initial point

--- src/ReportGeometryDrawer/index.js
+++ src/ReportGeometryDrawer/index.js
@@ -1,6 +1,8 @@
 import React, { memo, useCallback, useContext, useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
+import { LAYER_IDS } from '../MapDrawingTools/MapLayers';
+import { MapContext } from '../App';
 import { MapDrawingToolsContext } from '../MapDrawingTools/ContextProvider';
 import { setIsPickingLocation } from '../ducks/map-ui';
 
@@ -8,8 +10,12 @@ import Footer from './Footer';
 import ReportOverview from './ReportOverview';
 import MapDrawingTools from '../MapDrawingTools';
 
+const TIMEOUT_TO_REMOVE_REDUNDANT_POINT = 150;
+
 const ReportGeometryDrawer = () => {
   const dispatch = useDispatch();
+
+  const map = useContext(MapContext);
 
   const { setMapDrawingData } = useContext(MapDrawingToolsContext);
 
@@ -23,6 +29,17 @@ const ReportGeometryDrawer = () => {
     setGeometryPoints(newPoints);
     geoJson.current = newGeoJson;
   }, []);
+
+  const onClickPoint = useCallback((event) => {
+    if (geometryPoints.length > 2) {
+      const isInitialPointClicked = !!map.queryRenderedFeatures(event.point, { layers: [LAYER_IDS.POINTS] })
+        .find((point) => point.properties.pointIndex === 0);
+      if (isInitialPointClicked) {
+        setTimeout(() => setGeometryPoints(geometryPoints), TIMEOUT_TO_REMOVE_REDUNDANT_POINT);
+        setIsDrawing(false);
+      }
+    }
+  }, [geometryPoints, map]);
 
   const onSaveGeometry = useCallback(() => {
     setMapDrawingData(geoJson.current);
@@ -50,7 +67,12 @@ const ReportGeometryDrawer = () => {
 
   return <>
     <ReportOverview />
-    <MapDrawingTools drawing={isDrawing} onChange={onChangeGeometry} points={geometryPoints} />
+    <MapDrawingTools
+      drawing={isDrawing}
+      onChange={onChangeGeometry}
+      onClickPoint={onClickPoint}
+      points={geometryPoints}
+    />
     <Footer disableSaveButton={isDrawing} onSave={onSaveGeometry} />
   </>;
 };
