fd98da1af405986cd42a52364c934a748afa669c
Ludwig <luixlive@gmail.com>
2022-11-17 14:25:24 -0600

    ERA-7708: Add full on / off / call binding implementations for the map mock

--- src/ReportGeometryDrawer/index.test.js
+++ src/ReportGeometryDrawer/index.test.js
@@ -17,21 +17,21 @@ jest.mock('../ducks/map-ui', () => ({
   setIsPickingLocation: jest.fn(),
 }));
 
-describe('ReportGeometryDrawer', () => {
-  const geometryExample = {
-    type: 'Feature',
-    geometry: {
-      type: 'Polygon',
-      coordinates: [
-        [
-          [6.657425, 9.301125],
-          [-40.668725, 5.047775],
-          [5.0602, -13.74975]
-        ]
+const geometryExample = {
+  type: 'Feature',
+  geometry: {
+    type: 'Polygon',
+    coordinates: [
+      [
+        [6.657425, 9.301125],
+        [-40.668725, 5.047775],
+        [5.0602, -13.74975]
       ]
-    }
-  };
+    ]
+  }
+};
 
+describe('ReportGeometryDrawer', () => {
   const setMapDrawingData = jest.fn();
   let map, setIsPickingLocationMock, store;
   beforeEach(() => {
@@ -76,7 +76,7 @@ describe('ReportGeometryDrawer', () => {
 
   test('opens the cancellation confirmation modal when pressing Escape if user made a change', async () => {
     map.__test__.fireHandlers('click', { lngLat: { lng: 87, lat: 54 } });
-    jest.advanceTimersByTime(1000);
+    jest.runOnlyPendingTimers();
 
     expect((await screen.queryByText('Discard Changes'))).toBeNull();
 
@@ -87,7 +87,7 @@ describe('ReportGeometryDrawer', () => {
 
   test('opens the cancellation confirmation modal when clicking Cancel if user made a change', async () => {
     map.__test__.fireHandlers('click', { lngLat: { lng: 87, lat: 54 } });
-    jest.advanceTimersByTime(1000);
+    jest.runOnlyPendingTimers();
 
     expect((await screen.queryByText('Discard Changes'))).toBeNull();
 
@@ -121,6 +121,115 @@ describe('ReportGeometryDrawer', () => {
 
   test('disables the save button while the user has not close the polygon', async () => {
     expect((await screen.findByText('Save'))).toHaveClass('disabled');
+  });
+
+  test('enables the save button if user clicks enter after drawing a polygon', async () => {
+    map.__test__.fireHandlers('click', { lngLat: { lng: 87, lat: 54 } });
+    jest.runOnlyPendingTimers();
+    map.__test__.fireHandlers('click', { lngLat: { lng: 88, lat: 54 } });
+    jest.runOnlyPendingTimers();
+    map.__test__.fireHandlers('click', { lngLat: { lng: 88, lat: 55 } });
+    jest.runOnlyPendingTimers();
+
+    const saveButton = await screen.findByText('Save');
+
+    expect(saveButton).toHaveClass('disabled');
+
+    userEvent.keyboard('{Enter}');
+
+    expect(saveButton).not.toHaveClass('disabled');
+  });
+
+  test('enables the save button if user double clicks the map after drawing a polygon', async () => {
+    map.__test__.fireHandlers('click', { lngLat: { lng: 87, lat: 54 } });
+    jest.runOnlyPendingTimers();
+    map.__test__.fireHandlers('click', { lngLat: { lng: 88, lat: 54 } });
+    jest.runOnlyPendingTimers();
+    map.__test__.fireHandlers('click', { lngLat: { lng: 88, lat: 55 } });
+    jest.runOnlyPendingTimers();
+
+    const saveButton = await screen.findByText('Save');
+
+    expect(saveButton).toHaveClass('disabled');
+
+    map.__test__.fireHandlers('dblclick', { lngLat: { lng: 87, lat: 55 } });
+    jest.runOnlyPendingTimers();
+
+    await waitFor(() => {
+      expect(saveButton).not.toHaveClass('disabled');
+    });
+  });
+
+  test('enables the save button if user clicks the initial point after drawing a polygon', async () => {
+    map.__test__.fireHandlers('click', { lngLat: { lng: 87, lat: 54 } });
+    jest.runOnlyPendingTimers();
+    map.__test__.fireHandlers('click', { lngLat: { lng: 88, lat: 54 } });
+    jest.runOnlyPendingTimers();
+    map.__test__.fireHandlers('click', { lngLat: { lng: 88, lat: 55 } });
+    jest.runOnlyPendingTimers();
+
+    const saveButton = await screen.findByText('Save');
+
+    expect(saveButton).toHaveClass('disabled');
+
+    map.queryRenderedFeatures.mockImplementation(() => [{ properties: { pointIndex: 0 } }]);
+    map.__test__.fireHandlers('click', { lngLat: { lng: 87, lat: 54 } });
+    jest.runOnlyPendingTimers();
+
+    await waitFor(() => {
+      expect(saveButton).not.toHaveClass('disabled');
+    });
+  });
+
+  test('disables the save button if user closes an invalid polygon', async () => {
+    cleanup();
+    render(
+      <Provider store={mockStore(store)}>
+        <NavigationWrapper>
+          <MapContext.Provider value={map}>
+            <MapDrawingToolsContext.Provider value={{ setMapDrawingData }}>
+              <ReportGeometryDrawer />
+            </MapDrawingToolsContext.Provider>
+          </MapContext.Provider>
+        </NavigationWrapper>
+      </Provider>
+    );
+
+    map.__test__.fireHandlers('click', { lngLat: { lng: 87, lat: 54 } });
+    jest.runOnlyPendingTimers();
+    map.__test__.fireHandlers('click', { lngLat: { lng: 88, lat: 54 } });
+    jest.runOnlyPendingTimers();
+    map.__test__.fireHandlers('click', { lngLat: { lng: 88, lat: 55 } });
+    jest.runOnlyPendingTimers();
+    map.__test__.fireHandlers('click', { lngLat: { lng: 86, lat: 52 } });
+    jest.runOnlyPendingTimers();
+
+    const saveButton = await screen.findByText('Save');
+
+    expect(saveButton).toHaveClass('disabled');
+
+    userEvent.keyboard('{Enter}');
+
+    expect(saveButton).toHaveClass('disabled');
+  });
+
+  test('sets to false the pickingLocation flag when saving', async () => {
+    map.__test__.fireHandlers('click', { lngLat: { lng: 87, lat: 54 } });
+    jest.runOnlyPendingTimers();
+    map.__test__.fireHandlers('click', { lngLat: { lng: 88, lat: 54 } });
+    jest.runOnlyPendingTimers();
+    map.__test__.fireHandlers('click', { lngLat: { lng: 88, lat: 55 } });
+    jest.runOnlyPendingTimers();
+
+    userEvent.keyboard('{Enter}');
+
+    expect(setIsPickingLocation).toHaveBeenCalledTimes(0);
+
+    const saveButton = await screen.findByText('Save');
+    userEvent.click(saveButton);
+
+    expect(setIsPickingLocation).toHaveBeenCalledTimes(1);
+    expect(setIsPickingLocation).toHaveBeenCalledWith(false);
   });
 
   test('moves the map to geometry bbox if the event has already one', async () => {
