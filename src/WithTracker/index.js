f201ea95787abbd7f8d590beb12e2f76d559c69c
Ludwig <luixlive@gmail.com>
2023-05-11 09:42:36 -0600

    ERA-7643: Add react-ga4 library and configure to run along react-ga

--- src/WithTracker/index.js
+++ src/WithTracker/index.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
+import ReactGA4 from 'react-ga4';
 import { useLocation } from 'react-router-dom';
 
 const withTracker = (WrappedComponent, options = {}) => {
   const trackPage = page => {
-    ReactGA.set({
-      page,
-      ...options
-    });
+    ReactGA.set({ page, ...options });
     ReactGA.pageview(page);
+    ReactGA4.set({ page, ...options });
+    ReactGA4.send({ hitType: 'pageview', page });
   };
 
   const HOC = props => {
