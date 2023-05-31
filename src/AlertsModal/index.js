e22272c0a40c8048719040fbf08c0174afeea2dc
Ludwig <luixlive@gmail.com>
2023-05-31 12:55:15 -0600

    ERA-8583: Add alerts modal new message

--- src/AlertsModal/index.js
+++ src/AlertsModal/index.js
@@ -1,31 +1,36 @@
-import React, { Fragment, useState, memo } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { memo, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
+import PropTypes from 'prop-types';
 
 import { DAS_HOST } from '../constants';
-import { removeModal } from '../ducks/modals';
 import LoadingOverlay from '../LoadingOverlay';
 
-const { Header, Title, Body } = Modal;
+import styles from './styles.module.scss';
 
 const AlertsModal = ({ title }) => {
   const [loading, setLoadState] = useState(true);
 
-  return <Fragment>
+  return <>
     {loading && <LoadingOverlay />}
-    <Header closeButton>
-      <Title>{title}</Title>
-    </Header>
-    <Body>
-      <iframe title='Configure your EarthRanger alerts' src={`${DAS_HOST}/alerts`} onLoad={() => setLoadState(false)} />
-    </Body>
-  </Fragment>;
+
+    <Modal.Header closeButton>
+      <Modal.Title>{title}</Modal.Title>
+    </Modal.Header>
+
+    <Modal.Body>
+      <iframe
+        className={styles.alerts}
+        onLoad={() => setLoadState(false)}
+        src={`${DAS_HOST}/alerts`}
+        title='Configure your EarthRanger alerts'
+      />
+
+      {!loading && <p className={styles.message}>Each EarthRanger user can receive up to 20 alerts per day.</p>}
+    </Modal.Body>
+  </>;
 };
 
-AlertsModal.propTypes = {
-  title: PropTypes.string.isRequired,
-};
+AlertsModal.propTypes = { title: PropTypes.string.isRequired };
 
 
-export default connect(null, { removeModal })(memo(AlertsModal));
+export default memo(AlertsModal);
