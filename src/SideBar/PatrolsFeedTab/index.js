1a02a59dc68e162a9ef191bfa7da14eda699c1cf
Gabriel López <gabodev@Gabriels-MacBook-Pro-2.local>
2023-06-15 15:18:38 -0600

    Reverting file

--- src/SideBar/PatrolsFeedTab/index.js
+++ src/SideBar/PatrolsFeedTab/index.js
@@ -2,20 +2,18 @@ import React, { memo, useCallback, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 import { getPatrolList } from '../../selectors/patrols';
+import useNavigate from '../../hooks/useNavigate';
 
 import PatrolFilter from '../../PatrolFilter';
 import PatrolList from '../../PatrolList';
 import { sortPatrolList } from '../../utils/patrols';
-import useNavigate from '../../hooks/useNavigate';
 
 const PatrolsFeedTab = ({ loadingPatrolsFeed }) => {
+  const navigate= useNavigate();
   const patrols = useSelector(getPatrolList);
   const sortedPatrols = useMemo(() => sortPatrolList(patrols.results), [patrols.results]);
-  const navigate = useNavigate();
 
-  const onItemClick = useCallback((id) => {
-    navigate(id);
-  }, [navigate]);
+  const onItemClick = useCallback((id) => navigate(id), [navigate]);
 
   return <>
     <PatrolFilter />
