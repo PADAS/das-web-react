5fd913af380ca348c2faf0331069a3fd8f0c3c53
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2023-11-05 06:05:39 +0200

    fix:preserving featured device status properties by key-driven array reconciliation, to avoid losing the 'default: true' prop on device status props

--- src/ducks/subjects.js
+++ src/ducks/subjects.js
@@ -1,6 +1,7 @@
 import axios, { CancelToken } from 'axios';
 import union from 'lodash/union';
 import merge from 'lodash/merge';
+import keyBy from 'lodash/keyBy';
 
 import { API_URL } from '../constants';
 import globallyResettableReducer from '../reducers/global-resettable';
@@ -184,14 +185,28 @@ export const subjectStoreReducer = globallyResettableReducer((state = SUBJECT_ST
   }
 
   if (type === SOCKET_SUBJECT_STATUS) {
+    const { properties: { id } } = payload;
+
+    const subjectFromState = state[id];
+
+    if (!subjectFromState) return state;
+
     console.log('realtime: subject update', payload);
 
     const cloned = { ...payload };
+
+    if (subjectFromState.device_status_properties
+      && payload.device_status_properties
+    ) {
+      cloned.device_status_properties =  Object.values(
+        merge(
+          keyBy(subjectFromState.device_status_properties, 'label'),
+          keyBy(cloned.device_status_properties, 'label')
+        )
+      );
+    }
+
     cloned.properties.image = calcUrlForImage(cloned.properties.image);
-
-    const { properties: { id } } = cloned;
-
-    if (!state[id]) return state;
 
     const updatedSubject = updateSubjectLastPositionFromSocketStatusUpdate(state[id], cloned);
 
