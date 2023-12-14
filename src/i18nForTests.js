8dd9604ce9e9870e3d9216ae1dc4189f4400b1d4
Ludwig <luixlive@gmail.com>
2023-12-14 10:14:59 -0600

    ERA-9193: Fix import in i18nForTests

--- src/i18nForTests.js
+++ src/i18nForTests.js
@@ -1,23 +1,23 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import loginEn from '../public/locales/en/login.json';
-import sideBarEn from '../public/locales/en/side-bar.json';
+import loginEnUS from '../public/locales/en-US/login.json';
+import sideBarEnUS from '../public/locales/en-US/side-bar.json';
 
 i18n
   .use(initReactI18next)
   .init({
-    lng: 'en',
-    fallbackLng: 'en',
+    lng: 'en-US',
+    fallbackLng: 'en-US',
 
     interpolation: {
       escapeValue: false, // not needed for react!!
     },
 
     resources: {
-      en: {
-        'login': loginEn,
-        'side-bar': sideBarEn,
+      'en-US': {
+        'login': loginEnUS,
+        'side-bar': sideBarEnUS,
       },
     },
   });
