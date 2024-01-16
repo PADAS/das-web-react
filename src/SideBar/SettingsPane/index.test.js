54630abddc0a18c89c053a2eec9602767a99376f
Ludwig <luixlive@gmail.com>
2024-01-16 17:14:35 -0600

    ERA-9303: Fix i18next warning in tests

--- src/SideBar/SettingsPane/index.test.js
+++ src/SideBar/SettingsPane/index.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { MapContext } from '../../App';
 import { createMapMock } from '../../__test-helpers/mocks';
-import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import SettingsPane from './';
+
+import { render, screen } from '../../test-utils';
 
 import { mockStore } from '../../__test-helpers/MockStore';
 
