7f77156b3536d83a8e6b687a46d7ccda05a38a53
Gabriel López <gabodev@Gabriels-MacBook-Pro-2.local>
2024-02-15 15:26:10 -0600

    Adding todo tag

--- src/utils/locales.js
+++ src/utils/locales.js
@@ -1,6 +1,7 @@
 import esLang from 'date-fns/locale/es';
 import enUSLang from 'date-fns/locale/en-US';
 
+/** ToDO: Remove nullish operators after issues with import locales are fixed **/
 const es = esLang.es ?? esLang;
 const enUS = enUSLang.enUS ?? enUSLang;
 
