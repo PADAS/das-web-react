acee04626312e0284640b178ab8801ecbf2659cf
Ludwig <luixlive@gmail.com>
2024-02-26 13:50:29 -0600

    ERA-9382: Remove testing translations and version and UI fixes

--- src/i18n.js
+++ src/i18n.js
@@ -51,7 +51,7 @@ i18n
         expirationTime: 24 * 60 * 60 * 1000 * 7,
         defaultVersion: 'v0.1',
         versions: {
-          es: '0.2',
+          es: '0.1',
           'en-US': '0.1',
         }
       }]
