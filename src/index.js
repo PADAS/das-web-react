15dbc2e3db3041c1623470cb6581fd5b3ca7587b
Ludwig <luixlive@gmail.com>
2024-03-06 13:11:05 -0600

    ERA-9485: Set HTML lang attribute depending on i18n

--- src/index.js
+++ src/index.js
@@ -1,40 +1,32 @@
 import React, { lazy, Suspense, useEffect, useRef } from 'react';
-import { createRoot } from 'react-dom/client';
-import ReactGA4 from 'react-ga4';
-import { Provider } from 'react-redux';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { persistStore } from 'redux-persist';
+import { createRoot } from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+import { Provider } from 'react-redux';
+import ReactGA4 from 'react-ga4';
+import { useTranslation } from 'react-i18next';
 
-import JiraSupportWidget from './JiraSupportWidget';
-import GeoLocationWatcher from './GeoLocationWatcher';
-
-import store from './store';
-
-import {
-  EXTERNAL_SAME_DOMAIN_ROUTES,
-  REACT_APP_ROUTE_PREFIX,
-  REACT_APP_GA4_TRACKING_ID,
-} from './constants';
-
-import registerServiceWorker from './registerServiceWorker';
-import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+import './i18n';
 import './index.scss';
 
+import { EXTERNAL_SAME_DOMAIN_ROUTES, REACT_APP_GA4_TRACKING_ID, REACT_APP_ROUTE_PREFIX } from './constants';
+import registerServiceWorker from './registerServiceWorker';
+import { setClientReleaseIdentifier } from './utils/analytics';
+import store from './store';
 import withTracker from './WithTracker';
 
 import DetectOffline from './DetectOffline';
-import RequestConfigManager from './RequestConfigManager';
-
-import { setClientReleaseIdentifier } from './utils/analytics';
-
+import GeoLocationWatcher from './GeoLocationWatcher';
+import JiraSupportWidget from './JiraSupportWidget';
 import LoadingOverlay from './EarthRangerIconLoadingOverlay';
 import NavigationContextProvider from './NavigationContextProvider';
+import RequestConfigManager from './RequestConfigManager';
 import RequireAccessToken from './RequireAccessToken';
 import RequireEulaConfirmation from './RequireEulaConfirmation';
-
-import  './i18n';
 
 const App = lazy(() => import('./App'));
 const EulaPage = lazy(() => import('./views/EULA'));
@@ -44,12 +36,9 @@ const AppWithTracker = withTracker(App, 'EarthRanger');
 const EulaPageWithTracker = withTracker(EulaPage, 'EULA');
 const LoginWithTracker = withTracker(Login, 'Login');
 
-
-
 ReactGA4.initialize(REACT_APP_GA4_TRACKING_ID, { testMode: process.env.NODE_ENV === 'test' });
-setClientReleaseIdentifier();
 
-const persistor = persistStore(store);
+setClientReleaseIdentifier();
 
 const PathNormalizationRouteComponent = ({ location }) => {
   const externalRedirectRef = useRef(null);
@@ -62,42 +51,52 @@ const PathNormalizationRouteComponent = ({ location }) => {
   if (process.env.NODE_ENV !== 'production' || !localMatch) {
     return <Navigate replace to={REACT_APP_ROUTE_PREFIX} />;
   }
-
-  return <a href={localMatch} style={{ opacity: 0 }} target='_self' ref={externalRedirectRef}>{localMatch}</a>;
+  return <a href={localMatch} ref={externalRedirectRef} style={{ opacity: 0 }} target="_self">{localMatch}</a>;
 };
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const RootApp = () => {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    document.documentElement.lang = i18n.language;
+  }, [i18n.language]);
+
+  return <>
+    <RequestConfigManager />
+
+    <Suspense fallback={<LoadingOverlay />}>
+      <Routes>
+        <Route path={`${REACT_APP_ROUTE_PREFIX}login`} element={<LoginWithTracker />} />
+
+        <Route
+          path={`${REACT_APP_ROUTE_PREFIX}eula`}
+          element={<RequireAccessToken>
+            <EulaPageWithTracker />
+          </RequireAccessToken>}
+        />
+
+        <Route
+          path={`${REACT_APP_ROUTE_PREFIX}*`}
+          element={<RequireAccessToken>
+            <RequireEulaConfirmation>
+              <AppWithTracker />
+            </RequireEulaConfirmation>
+          </RequireAccessToken>}
+        />
+
+        <Route path="*" element={<PathNormalizationRouteComponent />} />
+      </Routes>
+    </Suspense>
+  </>;
+};
+
+const root = createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor} >
+    <PersistGate loading={null} persistor={persistStore(store)} >
       <BrowserRouter>
         <NavigationContextProvider>
-          <RequestConfigManager />
-
-          <Suspense fallback={<LoadingOverlay />}>
-            <Routes>
-              <Route path={`${REACT_APP_ROUTE_PREFIX}login`} element={<LoginWithTracker />} />
-
-              <Route
-                path={`${REACT_APP_ROUTE_PREFIX}eula`}
-                element={<RequireAccessToken>
-                  <EulaPageWithTracker />
-                </RequireAccessToken>}
-              />
-
-              <Route
-                path={`${REACT_APP_ROUTE_PREFIX}*`}
-                element={<RequireAccessToken>
-                  <RequireEulaConfirmation>
-                    <AppWithTracker />
-                  </RequireEulaConfirmation>
-                </RequireAccessToken>}
-              />
-
-              <Route path="*" element={<PathNormalizationRouteComponent />} />
-            </Routes>
-          </Suspense>
+          <RootApp />
         </NavigationContextProvider>
       </BrowserRouter>
 
