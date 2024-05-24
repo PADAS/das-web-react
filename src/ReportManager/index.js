f307b12162a97a94f1288eb76e40b7b1240fdade
Ludwig <luixlive@gmail.com>
2024-05-24 12:37:56 -0600

    Add include_updates=false to the feed fetch and load every single report when opening its page

--- src/ReportManager/index.js
+++ src/ReportManager/index.js
@@ -17,6 +17,7 @@ import useNavigate from '../hooks/useNavigate';
 import { uuid } from '../utils/string';
 
 import DelayedUnmount from '../DelayedUnmount';
+import LoadingOverlay from '../LoadingOverlay';
 import ReportDetailView from './ReportDetailView';
 
 import styles from './styles.module.scss';
@@ -90,10 +91,10 @@ const ReportManager = ({ onReportBeingAdded }) => {
   }, [onCancelAddedReport]);
 
   useEffect(() => {
-    if (isNewReport || eventStore[reportId]) {
+    if (isNewReport) {
       setIsLoadingReport(false);
     }
-  }, [eventStore, isNewReport, reportId]);
+  }, [isNewReport]);
 
   useEffect(() => {
     if (isNewReport) {
@@ -109,16 +110,16 @@ const ReportManager = ({ onReportBeingAdded }) => {
   }, [isNewReport, location.pathname, location.search, location.state, navigate, newReportTemporalId, reportType]);
 
   useEffect(() => {
-    if (!isNewReport && !eventStore[reportId]) {
+    if (!isNewReport) {
       setIsLoadingReport(true);
       dispatch(fetchEvent(reportId))
         .then(() => setIsLoadingReport(false))
         .catch(() => navigate(`/${TAB_KEYS.EVENTS}`, { replace: true }));
     }
-  }, [dispatch, eventStore, isNewReport, navigate, reportId]);
+  }, [dispatch, isNewReport, navigate, reportId]);
 
   return <TrackerContext.Provider value={reportTracker}>
-    {shouldRenderReportDetailView && <ReportDetailView
+    {shouldRenderReportDetailView ? <ReportDetailView
       formProps={navigationData?.formProps}
       isNewReport={isNewReport}
       key={reportId} // This resets component state when the id changes
@@ -126,7 +127,7 @@ const ReportManager = ({ onReportBeingAdded }) => {
       onAddReport={onAddReport}
       reportData={reportData}
       reportId={reportId}
-    />}
+    /> : <LoadingOverlay />}
 
     <DelayedUnmount isMounted={showAddedReport}>
       <ReportDetailView
