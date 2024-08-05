23fd55bfdb44b4257accaf3991dcdc238e02ac99
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2024-08-05 10:28:43 -0700

    adding exclude_empty_patrols param to patrols API requests

--- src/utils/patrol-filter.test.js
+++ src/utils/patrol-filter.test.js
@@ -1,7 +1,15 @@
 import { INITIAL_FILTER_STATE } from '../ducks/patrol-filter';
-import { isFilterModified } from './patrol-filter';
+import { isFilterModified, calcPatrolFilterForRequest } from './patrol-filter';
 
 describe('Patrol filter utils', () => {
+  describe('calcPatrolFilterForRequest', () => {
+    test('adds a constant exclude_empty_patrols=true param to all patrols API requests', () => {
+      const value = calcPatrolFilterForRequest({ hello: false });
+
+      expect(value).toContain('exclude_empty_patrols=true');
+    });
+  });
+
   describe('isFilterModified', () => {
     test('returns false if the filter has the default values', () => {
       expect(isFilterModified(INITIAL_FILTER_STATE)).toBe(false);
