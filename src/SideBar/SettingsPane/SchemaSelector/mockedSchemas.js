1b3421c8ed8b46546f617ca13f4260ffdc512075
Gabriel López <gabodev@Gabriels-MacBook-Pro-2.local>
2024-11-25 15:54:25 -0600

    Adding a more testable mocked schema

--- src/SideBar/SettingsPane/SchemaSelector/mockedSchemas.js
+++ src/SideBar/SettingsPane/SchemaSelector/mockedSchemas.js
@@ -1,61 +1,130 @@
 const schemaWithBasicText = {
-  label: 'Long and short text',
+  label: 'Long and short text configs',
   schema: {
     'json': {
       '$schema': 'https://json-schema.org/draft/2020-12/schema',
       'additionalProperties': false,
       'properties': {
-        'short_text': {
+        'required_short_text': {
           'default': '',
           'deprecated': false,
-          'description': 'This text will help you a lot in the field when you need extra information about the event.',
-          'title': 'Short text',
+          'description': '',
+          'title': 'Required short text',
           'type': 'string'
         },
-        'long_text': {
+        'required_long_text': {
           'default': '',
           'deprecated': false,
-          'description': 'Please add more information about this long text stuff',
-          'title': 'Long text',
+          'description': '',
+          'title': 'Required long text',
+          'type': 'string'
+        },
+        'short_text_with_a_hint': {
+          'default': '',
+          'deprecated': false,
+          'description': '',
+          'title': 'Short text with a hint',
+          'type': 'string'
+        },
+        'long_text_with_a_hint': {
+          'default': '',
+          'deprecated': false,
+          'description': '',
+          'title': 'Long text with a hint',
+          'type': 'string'
+        },
+        'short_text_with_description': {
+          'default': '',
+          'deprecated': false,
+          'description': 'These are some instructions for the data collector',
+          'title': 'Short text with description',
+          'type': 'string'
+        },
+        'long_text_with_description': {
+          'default': '',
+          'deprecated': false,
+          'description': 'These are some instructions for the data collector, these are some instructions for the data collector',
+          'title': 'Long text with description',
           'type': 'string'
         }
       },
       'required': [
-        'short_text'
+        'required_short_text',
+        'required_long_text'
       ],
       'type': 'object'
     },
     'ui': {
       'fields': {
-        'short_text': {
+        'required_short_text': {
           'inputType': 'SHORT_TEXT',
-          'placeholder': 'Ex. of data',
+          'placeholder': '',
           'type': 'TEXT',
-          'parent': 'section-GQi_CTfZ9ZH-CcSnr-OKa'
+          'parent': 'section-qTr7if4PZeQ2tOD5HKTfI'
         },
-        'long_text': {
+        'required_long_text': {
           'inputType': 'LONG_TEXT',
-          'placeholder': 'This is an example of long text',
+          'placeholder': '',
           'type': 'TEXT',
-          'parent': 'section-GQi_CTfZ9ZH-CcSnr-OKa'
+          'parent': 'section-qTr7if4PZeQ2tOD5HKTfI'
+        },
+        'short_text_with_a_hint': {
+          'inputType': 'SHORT_TEXT',
+          'placeholder': 'This is a hint',
+          'type': 'TEXT',
+          'parent': 'section-qTr7if4PZeQ2tOD5HKTfI'
+        },
+        'long_text_with_a_hint': {
+          'inputType': 'SHORT_TEXT',
+          'placeholder': 'This is an example of data',
+          'type': 'TEXT',
+          'parent': 'section-qTr7if4PZeQ2tOD5HKTfI'
+        },
+        'short_text_with_description': {
+          'inputType': 'SHORT_TEXT',
+          'placeholder': '',
+          'type': 'TEXT',
+          'parent': 'section-qTr7if4PZeQ2tOD5HKTfI'
+        },
+        'long_text_with_description': {
+          'inputType': 'LONG_TEXT',
+          'placeholder': '',
+          'type': 'TEXT',
+          'parent': 'section-qTr7if4PZeQ2tOD5HKTfI'
         }
       },
       'headers': {},
       'order': [
-        'section-GQi_CTfZ9ZH-CcSnr-OKa'
+        'section-qTr7if4PZeQ2tOD5HKTfI'
       ],
       'sections': {
-        'section-GQi_CTfZ9ZH-CcSnr-OKa': {
+        'section-qTr7if4PZeQ2tOD5HKTfI': {
           'columns': 1,
           'isActive': true,
           'label': '',
           'leftColumn': [
             {
-              'name': 'short_text',
+              'name': 'required_short_text',
               'type': 'field'
             },
             {
-              'name': 'long_text',
+              'name': 'required_long_text',
+              'type': 'field'
+            },
+            {
+              'name': 'short_text_with_a_hint',
+              'type': 'field'
+            },
+            {
+              'name': 'long_text_with_a_hint',
+              'type': 'field'
+            },
+            {
+              'name': 'short_text_with_description',
+              'type': 'field'
+            },
+            {
+              'name': 'long_text_with_description',
               'type': 'field'
             }
           ],
