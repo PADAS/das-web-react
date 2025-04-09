af9cb754c998ee2c5b8040f5e9da9fe71f2f2eec
Ludwig <luixlive@gmail.com>
2025-04-09 12:12:02 -0600

    ERA-11293: Rename scripts and config folders and fix eslint parser babel-preset-react-app

--- react-app-config/paths.js
+++ react-app-config/paths.js
@@ -50,7 +50,7 @@ const resolveModule = (resolveFn, filePath) => {
   return resolveFn(`${filePath}.js`);
 };
 
-// config after eject: we're in ./config/
+// config after eject: we're in ./react-app-config/
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
--- react-scripts/build.js
+++ react-scripts/build.js
@@ -12,15 +12,15 @@ process.on('unhandledRejection', err => {
 });
 
 // Ensure environment variables are read.
-require('../config/env');
+require('../react-app-config/env');
 
 const path = require('path');
 const chalk = require('react-dev-utils/chalk');
 const fs = require('fs-extra');
 const bfj = require('bfj');
 const webpack = require('webpack');
-const configFactory = require('../config/webpack.config');
-const paths = require('../config/paths');
+const configFactory = require('../react-app-config/webpack.config');
+const paths = require('../react-app-config/paths');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
