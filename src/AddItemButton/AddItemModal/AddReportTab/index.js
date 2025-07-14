4bce0bdba27496617b5c1f49df165b8287e8d5f7
Gabriel López <gabodev@Gabriels-MacBook-Pro-2.local>
2025-07-14 15:40:39 -0600

    Adding default behavior to jump-to-event select

--- src/AddItemButton/AddItemModal/AddReportTab/index.js
+++ src/AddItemButton/AddItemModal/AddReportTab/index.js
@@ -84,6 +84,8 @@ const AddReportTab = ({ onHideModal }) => {
         getOptionLabel={(option) => option.display}
         isSearchable
         onChange={onQuickJumpSelectChange}
+        menuPortalTarget={null}
+        menuPlacement='bottom'
         options={eventsByCategory}
         placeholder={t('reportsSelectPlaceholder')}
       />
