374d78b52403451ad15e3b1dfc14b0bbe159c277
Ludwig <luixlive@gmail.com>
2025-07-17 13:56:13 -0600

    ERA-1766: Add missing test

--- src/ducks/coordinate-reference-systems/index.test.js
+++ src/ducks/coordinate-reference-systems/index.test.js
@@ -88,5 +88,82 @@ describe('Ducks - Coordinate reference systems', () => {
 
       expect(coordinateReferenceSystemsReducer(INITIAL_STATE, action)).toEqual(expectedState);
     });
+
+    test('sorts the stored systems by their code when handling a SET_STORED_COORDINATE_REFERENCE_SYSTEMS action', async () => {
+      const payload = [{
+        area: 'Costa Rica - onshore and offshore east of 86°30\'W.',
+        bbox: [
+          11.77,
+          -86.5,
+          2.21,
+          -81.43
+        ],
+        code: '5367',
+        name: 'CR05 / CRTM05',
+        proj4: '+proj=tmerc +lat_0=0 +lon_0=-84 +k=0.9999 +x_0=500000 +y_0=0 +ellps=WGS84 +towgs84=-0.16959,0.35312,0.51846,-0.03385,0.16325,-0.03446,0.03693 +units=m +no_defs +type=crs',
+      }, {
+        area: 'United States (USA) - Colorado - counties Adams; Boulder; Gilpin; Grand; Jackson; Larimer; Logan; Moffat; Morgan; Phillips; Rio Blanco; Routt; Sedgwick; Washington; Weld; Yuma.',
+        bbox: [
+          41.01,
+          -109.06,
+          39.56,
+          -102.04
+        ],
+        code: '26753',
+        name: 'NAD27 / Colorado North',
+        proj4: '+proj=lcc +lat_0=39.3333333333333 +lon_0=-105.5 +lat_1=39.7166666666667 +lat_2=40.7833333333333 +x_0=609601.219202438 +y_0=0 +ellps=clrk66 +nadgrids=NTv2_0.gsb +units=us-ft +no_defs +type=crs',
+      }, {
+        area: 'China - onshore between 120°E and 126°E.',
+        bbox: [
+          53.56,
+          120,
+          26.34,
+          126
+        ],
+        code: '4576',
+        name: 'New Beijing / Gauss-Kruger zone 21',
+        proj4: '+proj=tmerc +lat_0=0 +lon_0=123 +k=1 +x_0=21500000 +y_0=0 +ellps=krass +units=m +no_defs +type=crs',
+      }];
+      const action = { payload, type: SET_STORED_COORDINATE_REFERENCE_SYSTEMS };
+      const expectedState = {
+        selectedSystems: Object.values(GPS_FORMATS),
+        storedSystems: [{
+          area: 'China - onshore between 120°E and 126°E.',
+          bbox: [
+            53.56,
+            120,
+            26.34,
+            126
+          ],
+          code: '4576',
+          name: 'New Beijing / Gauss-Kruger zone 21',
+          proj4: '+proj=tmerc +lat_0=0 +lon_0=123 +k=1 +x_0=21500000 +y_0=0 +ellps=krass +units=m +no_defs +type=crs',
+        }, {
+          area: 'Costa Rica - onshore and offshore east of 86°30\'W.',
+          bbox: [
+            11.77,
+            -86.5,
+            2.21,
+            -81.43
+          ],
+          code: '5367',
+          name: 'CR05 / CRTM05',
+          proj4: '+proj=tmerc +lat_0=0 +lon_0=-84 +k=0.9999 +x_0=500000 +y_0=0 +ellps=WGS84 +towgs84=-0.16959,0.35312,0.51846,-0.03385,0.16325,-0.03446,0.03693 +units=m +no_defs +type=crs',
+        }, {
+          area: 'United States (USA) - Colorado - counties Adams; Boulder; Gilpin; Grand; Jackson; Larimer; Logan; Moffat; Morgan; Phillips; Rio Blanco; Routt; Sedgwick; Washington; Weld; Yuma.',
+          bbox: [
+            41.01,
+            -109.06,
+            39.56,
+            -102.04
+          ],
+          code: '26753',
+          name: 'NAD27 / Colorado North',
+          proj4: '+proj=lcc +lat_0=39.3333333333333 +lon_0=-105.5 +lat_1=39.7166666666667 +lat_2=40.7833333333333 +x_0=609601.219202438 +y_0=0 +ellps=clrk66 +nadgrids=NTv2_0.gsb +units=us-ft +no_defs +type=crs',
+        }],
+      };
+
+      expect(coordinateReferenceSystemsReducer(INITIAL_STATE, action)).toEqual(expectedState);
+    });
   });
 });
