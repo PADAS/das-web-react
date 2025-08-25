a229a5c184ff101edc4ace76e6330567ef2e21a3
Ludwig <luixlive@gmail.com>
2025-08-25 16:50:45 -0600

    ERA-11922: Limit the amount of added coordinate reference systems to 6

--- src/i18n.js
+++ src/i18n.js
@@ -7,7 +7,7 @@ import LocalStorageBackend from 'i18next-localstorage-backend';
 
 import { SUPPORTED_LANGUAGES } from './constants';
 
-const I18N_FILES_VERSION = '1.21';
+const I18N_FILES_VERSION = '1.22';
 
 const preloadNamespaces = [
   'components',
