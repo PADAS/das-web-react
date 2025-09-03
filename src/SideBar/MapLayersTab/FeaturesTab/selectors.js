95b8ed15eb4f00c38aae1dc8dda33de6fa2d2def
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2025-09-03 16:26:22 -0700

    structural tweaks to list all feature data in map layers without individual featureset queries.

--- src/SideBar/MapLayersTab/FeaturesTab/selectors.js
+++ src/SideBar/MapLayersTab/FeaturesTab/selectors.js
@@ -1,20 +1,32 @@
-import groupBy from 'lodash/groupBy';
 import { createSelector } from 'reselect';
 
 import { featureSets } from '../../../selectors';
 
 export const getFeatureLayerListState = createSelector(
   [(state, props) => featureSets(state, props)],
-  (featureSets) => featureSets.map((set) => {
-    const groupedFeatures = groupBy(set.geojson.features, 'properties.type_name');
-    const featuresByType = Object.entries(groupedFeatures).map(([name, features]) => ({
-      name,
-      features,
-    }));
-    return ({
-      name: set.name,
-      id: set.id,
-      featuresByType,
-    });
-  }),
+  (featureSets) => featureSets
+    .reduce((result, set, _index) => {
+      if (!set.types.length) return result;
+
+      const featuresByType = set.types
+        .reduce((accumulator, type, _index) => {
+          if (!type.feature_count) return accumulator;
+
+          accumulator.push({
+            name: type.name,
+            features: type.feature_summaries,
+          });
+
+          return accumulator;
+
+        }, []);
+
+      result.push({
+        name: set.name,
+        id: set.id,
+        featuresByType,
+      });
+
+      return result;
+    }, []),
 );
