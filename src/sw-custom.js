f8c16cdd4d0692e7e0d0f035acd74d6c9244afd9
Joshua Krautwurst <38018017+JoshuaVulcan@users.noreply.github.com>
2025-10-22 14:58:40 -0700

    debug:logging

--- src/sw-custom.js
+++ src/sw-custom.js
@@ -176,6 +176,10 @@ if ('function' === typeof importScripts) {
                     if (etag) headers['If-None-Match'] = etag;
                     if (lastModified) headers['If-Modified-Since'] = lastModified;
 
+                    // Debug: Check what auth header we have
+                    const authHeader = request.headers.get('Authorization');
+                    console.log('Auth header for conditional request:', authHeader ? 'Present' : 'Missing');
+                    
                     const conditionalRequest = new Request(request.url, {
                       method: 'GET',
                       headers: {
