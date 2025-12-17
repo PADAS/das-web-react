a47cc1c70315349c2b70a89a06384c3cb142baa8
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2025-12-17 09:52:02 -0800

    adding back realtime

--- src/withSocketConnection/useRealTimeImplementation/index.js
+++ src/withSocketConnection/useRealTimeImplementation/index.js
@@ -68,11 +68,10 @@ const useRealTimeImplementation = () => {
     });
     socket.on('resp_authorization', (msg) => {
       const { status } = msg;
-      if (status.code === 401) {
-        // return store.dispatch(clearAuth());
-      }
-
       console.log('realtime: authorized', msg);
+      if (status.code === 401) {
+        return store.dispatch(clearAuth());
+      }
 
       window.clearInterval(pingInterval);
       window.clearTimeout(pingTimeout);
