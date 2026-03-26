2b55d9b7d8041ad4230fc1b35d6965104ccf7e22
Luis Alfonso Chávez Abbadie <luixlive@gmail.com>
2026-03-26 13:28:14 -0600

    Hotfix: Fix timezone for time fields in v2 forms

---
+++ src/ReportManager/DetailsSection/SchemaForm/utils/getFormDataWithFixedTimezones/index.js
@@ -0,0 +1,60 @@
+import { format, isValid, parseISO } from 'date-fns';
+
+import {
+  DATE_TIME_ELEMENT_INPUT_TYPES,
+  FORM_ELEMENT_TYPES,
+} from '../../../../../utils/v2-event-schemas/constants';
+
+const correctDateTimeValue = (value, inputType) => {
+  switch (inputType) {
+  case DATE_TIME_ELEMENT_INPUT_TYPES.DATE_TIME: {
+    const parsedDateTimeValue = parseISO(value);
+    return isValid(parsedDateTimeValue)
+      ? format(parsedDateTimeValue, 'yyyy-MM-dd\'T\'HH:mm:ssXXX')
+      : value;
+  }
+
+  case DATE_TIME_ELEMENT_INPUT_TYPES.TIME: {
+    const parsedTimeValue = parseISO(
+      `${format(new Date(), 'yyyy-MM-dd')}T${value}`,
+    );
+    return isValid(parsedTimeValue)
+      ? format(parsedTimeValue, 'HH:mm:ssXXX')
+      : value;
+  }
+
+  default:
+    return value;
+  }
+};
+
+const fixTimezoneRecursively = (formData, formElements) => {
+  const newFormData = {};
+
+  Object.entries(formData).forEach(([fieldId, fieldValue]) => {
+    switch (formElements[fieldId]?.type) {
+    case FORM_ELEMENT_TYPES.COLLECTION:
+      newFormData[fieldId] = fieldValue.map(
+        (item) => fixTimezoneRecursively(item, formElements)
+      );
+      break;
+
+    case FORM_ELEMENT_TYPES.DATE_TIME:
+      newFormData[fieldId] = correctDateTimeValue(
+        fieldValue,
+        formElements[fieldId].details.inputType
+      );
+      break;
+
+    default:
+      newFormData[fieldId] = fieldValue;
+    }
+  });
+
+  return newFormData;
+};
+
+const getFormDataWithFixedTimezones = (formData, formElements) =>
+  fixTimezoneRecursively(formData, formElements);
+
+export default getFormDataWithFixedTimezones;
