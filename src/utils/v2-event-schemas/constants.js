1503b89309eb6788cecb4507791ddd24af00206a
Luis Alfonso Chávez Abbadie <luixlive@gmail.com>
2026-06-01 15:36:04 -0600

    ERA-12362: Support for conditional sections' logical operators.
    - Updated the evaluateSectionConditions utility to support logical operators (AND, OR) for section conditions.
    - Modified the SchemaForm component to utilize the new logical operator functionality.
    - Added tests to verify the correct evaluation of section conditions based on the logical operators.
    - Introduced SECTION_ELEMENT_CONDITIONS_LOGICAL_OPERATORS constant for better maintainability.

--- src/utils/v2-event-schemas/constants.js
+++ src/utils/v2-event-schemas/constants.js
@@ -53,6 +53,11 @@ export const HEADER_ELEMENT_SIZES = {
 
 export const ROOT_CANVAS_ID = '__root__';
 
+export const SECTION_ELEMENT_CONDITIONS_LOGICAL_OPERATORS = {
+  AND: 'AND',
+  OR: 'OR',
+};
+
 export const TEXT_ELEMENT_ALPHANUMERIC_FORMAT_VALIDATION_PATTERN =
   '^[a-zA-Z0-9]+$';
 
--- src/utils/v2-event-schemas/transformSchemaToFormElements/transformSection/index.js
+++ src/utils/v2-event-schemas/transformSchemaToFormElements/transformSection/index.js
@@ -2,6 +2,7 @@ import {
   FORM_ELEMENT_LOGIC_CONDITION_OPERATORS,
   FORM_ELEMENT_TYPES,
   ROOT_CANVAS_ID,
+  SECTION_ELEMENT_CONDITIONS_LOGICAL_OPERATORS,
 } from '../../constants';
 import transformField from '../transformField';
 import transformHeader from '../transformHeader';
@@ -21,13 +22,15 @@ const transformSection = (sectionId, jsonSchema, uiSchema, formElements) => {
   // If the section has conditions, the JSON schema for the section children is
   // the "then" subschema of the conditional section JSON subschema. Otherwise,
   // it is the root JSON schema.
-  const sectionJSONSubschema =
-    uiSchema.sections[sectionId].conditions?.length > 0
+  const conditionalSectionJSONSubschema =
+    sectionUISchema.conditions?.length > 0
       ? jsonSchema.allOf.find(
         (conditionalSectionJSONSubschema) =>
           conditionalSectionJSONSubschema['x-section'] === sectionId,
-      ).then
-      : jsonSchema;
+      )
+      : null;
+  const sectionJSONSubschema =
+    conditionalSectionJSONSubschema?.then ?? jsonSchema;
 
   const leftColumnChildren = sectionUISchema.leftColumn ?? [];
   const rightColumnChildren = sectionUISchema.rightColumn ?? [];
@@ -77,6 +80,13 @@ const transformSection = (sectionId, jsonSchema, uiSchema, formElements) => {
           CONDITION_OPERATOR_MIGRATIONS[condition.operator] ??
           condition.operator,
       })),
+      // If there are conditions and they are combined with "anyOf", the
+      // conditionas logical operator is "OR". Otherwise, it is "AND".
+      conditionsLogicalOperator:
+        Array.isArray(conditionalSectionJSONSubschema?.if?.anyOf) &&
+        conditionalSectionJSONSubschema.if.anyOf.length > 0
+          ? SECTION_ELEMENT_CONDITIONS_LOGICAL_OPERATORS.OR
+          : SECTION_ELEMENT_CONDITIONS_LOGICAL_OPERATORS.AND,
       label: sectionUISchema.label ?? '',
       leftColumn: leftColumnActiveChildrenIds,
       rightColumn: rightColumnActiveChildrenIds,
