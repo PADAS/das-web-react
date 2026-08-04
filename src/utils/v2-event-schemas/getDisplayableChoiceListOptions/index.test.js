5b99588aea3e438ecf3fb8e539b9582ed1996ed8
Luis Alfonso Chávez Abbadie <luixlive@gmail.com>
2026-08-04 09:44:37 -0600

    ERA-13808: Stop sorting displayable choice list options alphabetically

--- src/utils/v2-event-schemas/getDisplayableChoiceListOptions/index.test.js
+++ src/utils/v2-event-schemas/getDisplayableChoiceListOptions/index.test.js
@@ -1,7 +1,7 @@
 import getDisplayableChoiceListOptions from '.';
 
 describe('getDisplayableChoiceListOptions', () => {
-  test('leaves only the descriptions for options whose displays are repeated and sorts the options by display', () => {
+  test('leaves only the descriptions for options whose displays are repeated, preserving the original order', () => {
     const options = [
       { value: 'v-zebra', display: 'Zebra', description: 'animal' },
       { value: 'v-ant-1', display: 'Ant', description: 'insect' },
@@ -12,14 +12,14 @@ describe('getDisplayableChoiceListOptions', () => {
       { value: 'v-fig-2', display: 'Fig' },
     ];
 
-    expect(getDisplayableChoiceListOptions(options, 'en')).toEqual([
-      { value: 'v-ant-1', display: 'Ant', description: 'insect' },
-      { value: 'v-ant-2', display: 'Ant', description: 'insect colony' },
-      { value: 'v-banana', display: 'Banana', description: '' },
-      { value: 'v-fig-1', display: 'Fig', description: 'fruit' },
-      { value: 'v-fig-2', display: 'Fig', description: undefined },
-      { value: 'v-mango', display: 'Mango', description: '' },
+    expect(getDisplayableChoiceListOptions(options)).toEqual([
       { value: 'v-zebra', display: 'Zebra', description: '' },
+      { value: 'v-ant-1', display: 'Ant', description: 'insect' },
+      { value: 'v-mango', display: 'Mango', description: '' },
+      { value: 'v-ant-2', display: 'Ant', description: 'insect colony' },
+      { value: 'v-fig-1', display: 'Fig', description: 'fruit' },
+      { value: 'v-banana', display: 'Banana', description: '' },
+      { value: 'v-fig-2', display: 'Fig', description: undefined },
     ]);
 
     expect(options).toEqual([
